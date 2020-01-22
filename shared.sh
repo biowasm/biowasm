d9cbfd7a0a966248a4d4ceafd303a3894bf9754c
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-01-21 20:17:03 -0800

    Update shared flags to support threads (will use this later on)

--- shared.sh
+++ shared.sh
@@ -1,13 +1,29 @@
 # Commonly used emcc compilation variables
 
-EM_FLAGS=$(cat <<EOF
+EM_FLAGS_BASE=$(cat <<EOF
     -s USE_ZLIB=1
     -s INVOKE_RUN=0
-    -s ALLOW_MEMORY_GROWTH=1
     -s FORCE_FILESYSTEM=1
     -s EXTRA_EXPORTED_RUNTIME_METHODS=["callMain"]
     -lworkerfs.js
 EOF
 )
 
+EM_FLAGS=$(cat <<EOF
+    ${EM_FLAGS_BASE}
+    -s ALLOW_MEMORY_GROWTH=1
+EOF
+)
+
+# Note: avoid using memory growth with threads (see https://emscripten.org/docs/porting/pthreads.html)
+EM_FLAGS_THREADS=$(cat <<EOF
+    ${EM_FLAGS_BASE}
+    -s USE_PTHREADS=1
+    -s PTHREAD_POOL_SIZE=10
+    -s TOTAL_MEMORY=100MB
+    --threadprofiler
+EOF
+)
+
 export EM_FLAGS;
+export EM_FLAGS_THREADS;
