24fda29fcd4a70c18f40bdcc4a21bed3e2bca273
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-01-21 20:19:54 -0800

    samtools: add bzip2 support + -O2

--- tools/samtools/compile.sh
+++ tools/samtools/compile.sh
@@ -11,15 +11,16 @@ make clean
 # Also, use autoheader/autoconf to generate config.h.in and configure
 autoheader
 autoconf -Wno-syntax
-emconfigure ./configure --without-curses --with-htslib="../../htslib/src/" CFLAGS="-s USE_ZLIB=1"
-emmake make CC=emcc AR=emar
+emconfigure ./configure --without-curses --with-htslib="../../htslib/src/" CFLAGS="-s USE_ZLIB=1 -s USE_BZIP2=1"
+emmake make CC=emcc AR=emar CFLAGS="-O2 -s USE_ZLIB=1 -s USE_BZIP2=1"
 
 # Rename output to .o so it's recognizable by Emscripten
 cp samtools samtools.o
 
-# Generate .wasm/.js files 
-emcc samtools.o \
+# Generate .wasm/.js files
+emcc -O2 samtools.o \
     -o ../build/samtools.html \
     $EM_FLAGS \
+    -s USE_BZIP2=1 \
     -s ERROR_ON_UNDEFINED_SYMBOLS=0 \
     --preload-file examples/@/tmp/examples/
