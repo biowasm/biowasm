e32d9ae5591b6447e1fb08775d2ae38b16ef3cee
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-01-25 08:11:18 -0800

    Add compile script for seq-align

---
+++ tools/seq-align/compile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd src/
+
+# Cleanup previous builds if any
+rm bin/*
+make clean
+git stash
+git submodule foreach 'git stash'
+
+# Replace -lz with -s USE_ZLIB=1
+find . -name 'Makefile' -exec sed -i 's/-lz/-s USE_ZLIB=1/g' {} \;
+# Replace ar with $(AR) so it picks up the Emscripten AR
+find . -name 'Makefile' -exec sed -i 's/ar / $(AR) /g' {} \;
+# Append -s USE_ZLIB=1 in all CFLAGS (don't want to overwrite CFLAGS because some have custom "-I" statements)
+find . -name 'Makefile' -exec sed -i '/^CFLAGS/ s/$/ -s USE_ZLIB=1/g' {} \;
+
+# Build
+emmake make
+
