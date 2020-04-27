4323ba0ab0128f133eda9feb10e4571d3d1e1388
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-04-26 20:21:47 -0700

    fastp: include a slightly larger sample FASTQ file in the preloaded files (30k reads, 3MB)

--- tools/fastp/compile.sh
+++ tools/fastp/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd src/
+curl -o "testdata/NA12878.fastq.gz" "https://data.sandbox.bio/fastq/NA12878.30k.fastq.gz"
 emmake make TARGET="fastp.o" LIBS="-s USE_ZLIB=1"
 em++ -O3 fastp.o \
     -o ../build/fastp.js \
