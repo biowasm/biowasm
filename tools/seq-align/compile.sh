fa0c7337c45bcb0e47b217b57954b626f76ccbf2
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-04-29 06:28:24 -0700

    seq-align: Generate js/wasm files for each tool in the build/ folder

--- tools/seq-align/compile.sh
+++ tools/seq-align/compile.sh
@@ -5,7 +5,6 @@ cd src/
 # Cleanup previous builds if any
 rm bin/*
 make clean
-git stash
 git submodule foreach 'git stash'
 
 # Replace -lz with -s USE_ZLIB=1
@@ -18,3 +17,15 @@ find . -name 'Makefile' -exec sed -i '/^CFLAGS/ s/$/ -s USE_ZLIB=1/g' {} \;
 # Build
 emmake make
 
+# Generate JS/Wasm for each tool
+em++ -O3 bin/needleman_wunsch.o \
+    -o ../build/needleman_wunsch.js \
+    $EM_FLAGS
+
+em++ -O3 bin/smith_waterman.o \
+    -o ../build/smith_waterman.js \
+    $EM_FLAGS
+
+em++ -O3 bin/lcs.o \
+    -o ../build/lcs.js \
+    $EM_FLAGS
