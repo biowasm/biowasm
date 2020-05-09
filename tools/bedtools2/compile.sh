6b2af1a6be2ffa3097c5d25ca3458579a674d20b
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-05-09 08:51:22 -0700

    bedtools2: add sample data (#8)
    * Delete large files (40MB) and preload the rest (~1.7MB)
    
    * Update bedtools to v2.29.2

--- tools/bedtools2/compile.sh
+++ tools/bedtools2/compile.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 cd src/
+
+# Remove large files (we'll pre-load the rest of the files as examples)
+rm -rf ./test/intersect/sortAndNaming/bigTests
+
+# Generate obj/*.o files
+make clean
 emmake make
 
 # Generate .wasm/.js files
 emcc obj/*.o \
-    -o ../build/bedtools2.html \
+    -o ../build/bedtools2.js \
+    -O2 \
+    --preload-file test@/bedtools2/test \
     $EM_FLAGS \
     -s ERROR_ON_UNDEFINED_SYMBOLS=0
