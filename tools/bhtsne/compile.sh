15031b896e41f4b096f93dc787b98bde613ba6ea
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-05-31 15:39:49 -0700

    Refactor so we don't need a patch file

--- tools/bhtsne/compile.sh
+++ tools/bhtsne/compile.sh
@@ -5,14 +5,15 @@ cd src/
 # Unzip data; we'll preload it with the module for convenience
 test -f brain8.snd || gunzip brain8.snd.gz
 
-# Compile to .wasm
+# Compile
+rm t-sne.o
 emmake make \
-    PROG="t-sne.html" \
     CC=emcc CXX=em++ \
     CFLAGS+="-s USE_ZLIB=1" \
-    LIBS="-s USE_ZLIB=1 -lm --preload-file brain8.snd"
+    LIBS="-s USE_ZLIB=1 -lm"
 
-# Move files to build folder
-for ext in data html js wasm; do
-    mv "t-sne.${ext}" ../build/
-done
+# Generate .wasm/.js files
+mv t-sne t-sne.o
+emcc -O2 -o ../build/t-sne.html t-sne.o \
+    $EM_FLAGS \
+    --preload-file brain8.snd@/bhtsne/brain8.snd
