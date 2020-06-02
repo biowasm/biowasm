f0d4754dce63b7b5f051e96ea8741679db0f1f2c
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-06-02 07:02:34 -0700

    Update compile.sh to enable Asyncify

--- tools/bhtsne/compile.sh
+++ tools/bhtsne/compile.sh
@@ -16,4 +16,7 @@ emmake make \
 mv t-sne t-sne.o
 emcc -O2 -o ../build/t-sne.html t-sne.o \
     $EM_FLAGS \
+    -s ASYNCIFY=1 \
+    -s 'ASYNCIFY_IMPORTS=["send_names","send_results"]' \
+    -s EXTRA_EXPORTED_RUNTIME_METHODS=["callMain","getValue","UTF8ToString"] \
     --preload-file brain8.snd@/bhtsne/brain8.snd
