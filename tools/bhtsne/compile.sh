189536df733f686851b80792f3979e4324d2be9c
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-06-05 07:12:04 -0700

    Update compile script to include sample data

--- tools/bhtsne/compile.sh
+++ tools/bhtsne/compile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd src/
-
-# Unzip data; we'll preload it with the module for convenience
-test -f brain8.snd || gunzip brain8.snd.gz
+# Unzip sample data; we'll preload it with the module for convenience
+test -f data/brain8.snd || gunzip data/brain8.snd.gz
+test -f data/pollen2014.snd || gunzip data/pollen2014.snd.gz
 
 # Compile
+cd src/
 rm t-sne.o
 emmake make \
     CC=emcc CXX=em++ \
@@ -19,4 +19,10 @@ emcc -O2 -o ../build/t-sne.html t-sne.o \
     -s ASYNCIFY=1 \
     -s 'ASYNCIFY_IMPORTS=["send_names","send_results"]' \
     -s EXTRA_EXPORTED_RUNTIME_METHODS=["callMain","getValue","UTF8ToString"] \
-    --preload-file brain8.snd@/bhtsne/brain8.snd
+    --preload-file ../data/brain8.snd@/bhtsne/brain8.snd \
+    --preload-file ../data/pollen2014.snd@/bhtsne/pollen2014.snd
+
+# Undo sample data unzipping
+cd -
+test -f data/brain8.snd.gz || gzip data/brain8.snd
+test -f data/pollen2014.snd.gz || gzip data/pollen2014.snd
