ca4aa1ded9591e0427ace62153226d6cee430b30
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-08-12 18:36:22 -0700

    Delete unused files and make example FASTQs smaller to save space

--- tools/bowtie2/compile.sh
+++ tools/bowtie2/compile.sh
@@ -2,6 +2,15 @@
 
 cd src/
 
+# Cloudflare currently has a 10MB/file limit so remove unneeded files
+rm example/reads/combined_reads.bam
+rm example/reads/longreads.fq
+
+cat example/reads/reads_1.fq | head -n4000 | gzip > example/reads/reads_1.fq.tmp
+cat example/reads/reads_2.fq | head -n4000 | gzip > example/reads/reads_2.fq.tmp
+mv example/reads/reads_1.fq.tmp example/reads/reads_1.fq
+mv example/reads/reads_2.fq.tmp example/reads/reads_2.fq
+
 # Build:
 #   - NO_TBB=1: disable Threading Building Blocks library
 #   - POPCNT_CAPABILITY=0: popcnt (used in bt2_idx.h) is an assembly instruction; not supported by WebAssembly
