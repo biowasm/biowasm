5ea3b2d488b50c5254529389acedf5a3449def0d
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-08-16 16:47:01 -0700

    Test using while read instead of for loop

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-TOOLS=(
-	"seqtk	1.3 v1.3"
+TOOLS=$(cat <<EOF
+	seqtk    1.3    v1.3
+EOF
 )
 
-for tool in ${TOOLS[@]};
+while read toolName toolVersion toolBranch;
 do
-	echo "==================================="
-	echo "Processing $tool"
-	echo "==================================="
+	echo "================================================================"
+	echo "Processing $toolName/$toolVersion @ $toolBranch"
+	echo "================================================================"
 
-	while read toolName toolVersion toolBranch;
-	do
-		# Go to branch/tag of interest (cleanup from previous iteration)
-		cd tools/${toolName}/src
-		git reset --hard
-		git clean -f -d
-		git fetch --all
-		git checkout "$toolBranch"
+	# Go to branch/tag of interest (cleanup from previous iteration)
+	cd tools/${toolName}/src
+	git reset --hard
+	git clean -f -d
+	git fetch --all
+	git checkout "$toolBranch"
 
-		# Build it
-		cd ../../..
-		make "$toolName"
-		ls -lah tools/${toolName}/build/
-		mkdir -p public/${toolName}/${toolVersion}/ public/${toolName}/latest/
-		cp tools/${toolName}/build/* public/${toolName}/${toolVersion}/
-		cp tools/${toolName}/build/* public/${toolName}/latest/
-	done <<< "$tool"
-done
+	# Build it
+	cd ../../..
+	make "$toolName"
+	ls -lah tools/${toolName}/build/
+	mkdir -p public/${toolName}/${toolVersion}/ public/${toolName}/latest/
+	cp tools/${toolName}/build/* public/${toolName}/${toolVersion}/
+	cp tools/${toolName}/build/* public/${toolName}/latest/
+done <<< "$TOOLS"
