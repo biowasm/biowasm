39335866ceb5764e76956f1bbbebb9e82a012756
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-08-16 10:44:54 -0700

    Cleanup pre.js

--- pre.js
+++ pre.js
@@ -1,15 +1,16 @@
 // Fetch the corresponding .wasm and .data files relative to where the .js file is stored,
 // whether it's locally or on a CDN. Works in the main thread and in WebWorkers.
-Module['locateFile'] = (path, dir) => {
-        var dirJS = "", dirRoot = "";
-        if(typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope)
-                dirJS = self.location.href;
-        else if(document.currentScript)
-                dirJS = document.currentScript.src;
+Module['locateFile'] = function(path, dir)
+{
+	var dirJS = "", dirRoot = "";
+	// Inside a WebWorker
+	if(typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope)
+		dirJS = self.location.href;
+	// Inside the main thread
+	else if(document.currentScript)
+		dirJS = document.currentScript.src;
 
-        dirRoot = dirJS.match(/.*\//)[0];
-
-        console.log(`path=${path}, dirRoot=${dirRoot}, dir=${dir}`)
-        return dirRoot + path
+	// Get folder where main .js file is located
+	dirRoot = dirJS.match(/.*\//)[0];
+	return dirRoot + path;
 }
-
