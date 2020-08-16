48a2a42bcb28a40a5c2caf55b3e245183a30e06f
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-08-16 15:12:49 -0700

    Allow user (and next version of Aioli) to hardcode URL to local folder or CDN

--- pre.js
+++ pre.js
@@ -1,17 +1,28 @@
 // Fetch the corresponding .wasm and .data files relative to where the .js file is stored,
 // whether it's locally or on a CDN. Works in the main thread and in WebWorkers.
 var Module = typeof Module !== 'undefined' ? Module : {};
+
 Module['locateFile'] = function(path, dir)
 {
-	var dirJS = "", dirRoot = "";
-	// Inside a WebWorker
-	if(typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope)
-		dirJS = self.location.href;
-	// Inside the main thread
-	else if(document.currentScript)
-		dirJS = document.currentScript.src;
+	var dirRoot = "";
 
-	// Get folder where main .js file is located
-	dirRoot = dirJS.substring(0, dirJS.lastIndexOf("/") + 1);
+	// Use hardcoded path for all files
+	if(typeof BIOWASM_URL !== 'undefined')
+		dirRoot = BIOWASM_URL;
+	
+	// Or infer it from the path to the JS file
+	else
+	{
+		var dirJS = "";
+		// Inside a WebWorker
+		if(typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope)
+			dirJS = self.location.href;
+		// In the main thread
+		else if(document.currentScript)
+			dirJS = document.currentScript.src;
+	
+		dirRoot = dirJS.substring(0, dirJS.lastIndexOf("/") + 1);
+	}
+
 	return dirRoot + path;
 }
