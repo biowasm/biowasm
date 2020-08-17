f392422dde56eb3c0772be565d05128e4e671e2e
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-08-16 17:16:06 -0700

    Add more tools

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
 TOOLS=$(cat <<EOF
-	seqtk    1.2    v1.2
-	seqtk    1.3    v1.3
+	seqtk        1.2           v1.2
+	seqtk        1.3           v1.3
+
+	fastp        0.20.1        0.20.1
+
+	seq-align    2017.10.18    dc41988
+
+	bhtsne       2016.08.22    1a62a5d
+
+	samtools     1.10          1.10
+
+	bedtools2    2.29.2        v2.29.2
 EOF
 )
 
 # Setup repos and dependencies
 make init
-sudo apt-get install -y tree liblzma-dev
+sudo -i
+apt-get install -y tree
 
 # Compile each tool
 while read toolName toolVersion toolBranch;
 do
+	if [[ "$toolName" == "" ]]; then
+		continue;
+	fi
+
 	echo "================================================================"
 	echo "Processing $toolName/$toolVersion @ $toolBranch"
 	echo "================================================================"
