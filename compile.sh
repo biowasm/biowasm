dc3a8087b30d31e52c84df2e30b8d6e32a4a21a0
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-09-30 21:08:59 -0700

    Test bundling Aioli onto the CDN

--- compile.sh
+++ compile.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# ------------------------------------------------------------------------------
+# Config
+# ------------------------------------------------------------------------------
+
+AIOLI=("1.0.0" "1.1.0" "1.1.3" "1.2.1" "1.3.0")
+
 # Format: toolName toolVersion toolBranch
 TOOLS=$(cat <<EOF
+	seqtk        1.2           v1.2
 	seqtk        1.3           v1.3
-	samtools     1.10          1.10
 EOF
 )
 
@@ -22,12 +28,15 @@ EOF
 
 	# wgsim        2011.10.17    a12da33
 
-
+# ------------------------------------------------------------------------------
 # Setup repos and dependencies
+# ------------------------------------------------------------------------------
 make init
 sudo apt-get install -y tree
 
+# ------------------------------------------------------------------------------
 # Compile each tool
+# ------------------------------------------------------------------------------
 while read toolName toolVersion toolBranch;
 do
 	if [[ "$toolName" == "" ]]; then
@@ -54,6 +63,26 @@ do
 	cp tools/${toolName}/build/* public/${toolName}/latest/
 done <<< "$TOOLS"
 
+# ------------------------------------------------------------------------------
+# Generate CDN files for Aioli
+# ------------------------------------------------------------------------------
+git clone "https://github.com/biowasm/aioli.git"
+cd aioli/
+for version in ${AIOLI[@]};
+do
+	git checkout "v$version"
+	dir_out="../public/aioli/$version"
+	mkdir -p "$dir_out/"
+	cp aioli{,.worker}.js "$dir_out/"
+done
+dir_out="../public/aioli/latest"
+mkdir -p "$dir_out/"
+cp aioli{,.worker}.js "$dir_out/"
+cd ../
+
+
+# ------------------------------------------------------------------------------
 # Generate index
+# ------------------------------------------------------------------------------
 cd public/
 ( tree --du -h | grep -v -E "index|index.html|.ico" | tail +2 ) > index
