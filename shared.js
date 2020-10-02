c1710611ab8a9ff51db3610200eacc11809fa5ae
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-10-02 07:39:53 -0700

    Shared JS: don't overwrite locateFile if it exists already

--- shared.js
+++ shared.js
@@ -1,28 +1,19 @@
-// Fetch the corresponding .wasm and .data files relative to where the .js file is stored,
-// whether it's locally or on a CDN. Works in the main thread and in WebWorkers.
-var Module = typeof Module !== 'undefined' ? Module : {};
+// Fetch the corresponding .wasm and .data files relative to where the .js file is stored.
+// Works in the main thread and in WebWorkers.
+var Module = typeof Module !== "undefined" ? Module : {};
 
-Module['locateFile'] = function(path, dir)
-{
-	var dirRoot = "";
-
-	// Use hardcoded path for all files
-	if(typeof BIOWASM_URL !== 'undefined')
-		dirRoot = BIOWASM_URL;
-	
-	// Or infer it from the path to the JS file
-	else
+if(typeof Module["locateFile"] == "undefined")
+	Module["locateFile"] = function(path, dir)
 	{
-		var dirJS = "";
+		var dirRoot = "", dirJS = "";
 		// Inside a WebWorker
-		if(typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope)
+		if(typeof WorkerGlobalScope !== "undefined" && self instanceof WorkerGlobalScope)
 			dirJS = self.location.href;
 		// In the main thread
 		else if(document.currentScript)
 			dirJS = document.currentScript.src;
 	
 		dirRoot = dirJS.substring(0, dirJS.lastIndexOf("/") + 1);
-	}
 
-	return dirRoot + path;
-}
+		return dirRoot + path;
+	}
