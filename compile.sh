06d0bea1aa71246e9886471cbf7b1ebf850f1cd7
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-10-24 15:43:48 -0700

    Cleanup

--- compile.sh
+++ compile.sh
@@ -7,8 +7,6 @@
 # Config
 # ------------------------------------------------------------------------------
 
-# FIXME:
-# AIOLI=("1.0.0" "1.1.0" "1.1.3" "1.2.1" "1.3.0")
 AIOLI=("1.4.0")
 
 # Format: toolName toolVersion toolBranch
@@ -29,21 +27,6 @@ TOOLS=$(cat <<EOF
 	wgsim        2011.10.17    a12da33
 EOF
 )
-
-	# bedtools2    2.29.2        v2.29.2
-
-	# bhtsne       2016.08.22    1a62a5d
-
-	# fastp        0.20.1        0.20.1
-
-	# samtools     1.10          1.10
-
-	# seq-align    2017.10.18    dc41988
-
-	# seqtk        1.2           v1.2
-	# seqtk        1.3           v1.3
-
-	# wgsim        2011.10.17    a12da33
 
 # ------------------------------------------------------------------------------
 # Setup repos and dependencies
@@ -87,7 +70,7 @@ git clone "https://github.com/biowasm/aioli.git"
 cd aioli/
 for version in ${AIOLI[@]};
 do
-	git checkout "issue-9-support-custom-paths"  # FIXME: git checkout "v$version"
+	git checkout "v$version"
 	dir_out="../public/aioli/$version"
 	mkdir -p "$dir_out/"
 	cp aioli{,.worker}.js "$dir_out/"
