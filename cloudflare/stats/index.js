fff29a713934d9e0258302a3290e985f6b912447
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-11-12 07:11:46 -0800

    Unify landing pages

--- cloudflare/stats/index.js
+++ cloudflare/stats/index.js
@@ -32,13 +32,72 @@ async function handleRequest(request)
   }
 
   return new Response(`
-        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-        <div id="plot"></div>
+        <!doctype html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta name="description" content="A repository of C/C++ genomics tools, pre-compiled to WebAssembly for use in a browser">
+            <title>biowasm.com</title>
+            <link rel="icon" href="favicon.ico">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+          </head>
+        
+          <body>
+            <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+              <div class="container">
+              <a class="navbar-brand" href="/">biowasm</a>
+              <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+        
+              <div id="navbar" class="collapse navbar-collapse">
+                <ul class="navbar-nav mr-auto"></ul>
+                <ul class="navbar-nav">
+                  <li class="nav-item mr-2">
+                    <a class="nav-link text-light-50" href="https://cdn.biowasm.com" target="_blank" onclick="javascript:window.location=window.location.href.includes('stg') ? 'https://cdn-stg.biowasm.com' : 'https://cdn.biowasm.com'; return false">Packages</a>
+                  </li>
+                  <li class="nav-item mr-2">
+                    <a class="nav-link text-light" href="https://stats.biowasm.com" target="_blank" onclick="javascript:window.location=window.location.href.includes('stg') ? 'https://stats-stg.biowasm.com' : 'https://stats.biowasm.com'; return false">Stats</a>
+                  </li>
+                  <li class="nav-item mr-3">
+                    <a class="nav-link text-light-50" href="https://github.com/biowasm" target="_blank">GitHub</a>
+                  </li>
+                  <!-- <li class="nav-item">
+                    <a class="btn btn-primary" href="https://play.biowasm.com" target="_blank">Launch playground</a>
+                  </li> -->
+                </ul>
+              </div>
+              </div>
+            </nav>
+        
+            <main role="main">
+              <div class="jumbotron mt-5 mt-md-2 pb-1">
+                <div class="container mt-4 mb-4">
+                  <h2>Stats</h2>
+                  <p class="lead">
+                    Package downloads over time
+                  </p>
+                </div>
+              </div>
+        
+              <div class="container">
+                <div class="row mt-4">
+                  <div class="col-md-12">
+  
+                    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+                    <div id="plot"></div>
 
-        <script type="application/javascript">
-        var data = ${JSON.stringify(data)};
-        Plotly.newPlot("plot", data);
-        </script>  
+                    <script type="application/javascript">
+                    var data = ${JSON.stringify(data)};
+                    Plotly.newPlot("plot", data);
+                    </script>
+                  </div>
+                </div>
+              </div>
+            </main>
+          </body>
+        </html>
     `, {
     headers: { "content-type": "text/html;charset=UTF-8" }
   });
