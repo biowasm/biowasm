06816aaf111b372ede16ffd010fd0b27878ca162
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-11-26 08:50:40 -0800

    Small update

--- cloudflare/playground/src/App.svelte
+++ cloudflare/playground/src/App.svelte
@@ -4,14 +4,16 @@
 // import Bootstrap from "bootstrap";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
+// Imports
 import CommandLine from "./CommandLine.svelte";
 
-
+// State
 let output = "";
 </script>
 
 <CommandLine
     command="samtools --version"
+    execute="true"
     on:output={msg => output = msg.detail.stdout.trim() + msg.detail.stderr.trim()} />
 
 <pre class="border rounded border-primary p-3" style="height:55vh">{output}</pre>
