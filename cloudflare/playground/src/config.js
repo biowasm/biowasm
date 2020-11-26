d86eb9e2d0d179b024afb9ad2d68e02575197edf
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-11-26 11:46:43 -0800

    Update samtools sample queries to another region

--- cloudflare/playground/src/config.js
+++ cloudflare/playground/src/config.js
@@ -1,6 +1,7 @@
 // Constants
 export const TOOLS = {
-	"samtools": {
+    // 9:22,000,000-22,100,000, 9:21,967,752-21,995,324
+    "samtools": {
 		aioli: { module: "samtools", version: "1.10" },
 		queries: [
             {
@@ -8,21 +9,27 @@ export const TOOLS = {
                 items: [
                     {
                         label: "samtools view",
-                        command: "samtools view /urls/A549_CDKN2A.bam 9:21,967,752-21,995,324",
+                        command: "samtools view /urls/A549_CDKN2A.bam 9:22,000,000-22,100,000",
                         tooltip: "Get reads from chr9",
                         description: "Get reads from a genomic region of interest"
                     },
                     {
-                        label: "samtools view -q 10",
-                        command: "samtools view -q 10 /urls/A549_CDKN2A.bam 9:21,967,752-21,995,324",
+                        label: "samtools view -q 20",
+                        command: "samtools view -q 20 /urls/A549_CDKN2A.bam 9:22,000,000-22,100,000",
                         tooltip: "Filter by mapping quality",
-                        description: "Filter out reads that have a mapping quality < 10 (mapping quality = 5th column)"
+                        description: "Filter out reads that have a mapping quality < 20 (mapping quality = 5th column of SAM file)"
                     },
                     {
-                        label: "samtools view -F 2",
-                        command: "samtools view -F 2 /urls/A549_CDKN2A.bam 9:21,967,752-21,995,324",
-                        tooltip: "Filter by SAM flags",
-                        description: "Filter out reads that don't satisfy a certain flag condition (flag = 2nd column)"
+                        label: "samtools view -F 4",
+                        command: "samtools view -F 4 /urls/A549_CDKN2A.bam 9:22,000,000-22,100,000",
+                        tooltip: "Filter out unmapped reads",
+                        description: "Use the <code>-F 4</code> flag to exclude reads where the SAM flag <code>4</code> is set (flag = 2nd column of SAM file) &mdash; <a href='https://broadinstitute.github.io/picard/explain-flags.html' target='_blank'>SAM flags utility</a>"
+                    },
+                    {
+                        label: "samtools view -f 4",
+                        command: "samtools view -f 4 /urls/A549_CDKN2A.bam 9:22,000,000-22,100,000",
+                        tooltip: "List unmapped reads",
+                        description: "Use the <code>-f 4</code> flag to only include reads where the SAM flag <code>4</code> is set (flag = 2nd column of SAM file) &mdash; <a href='https://broadinstitute.github.io/picard/explain-flags.html' target='_blank'>SAM flags utility</a>"
                     }
                 ]
             },
@@ -43,7 +50,7 @@ export const TOOLS = {
                     },
                     {
                         label: "samtools view -c",
-                        command: "samtools view -c /urls/A549_CDKN2A.bam 9:21,967,752-21,995,324",
+                        command: "samtools view -c /urls/A549_CDKN2A.bam 9:22,000,000-22,100,000",
                         tooltip: "Output number of reads",
                         description: "Only returns the number (or <u>c</u>ount) of reads in the region of interest"
                     }
