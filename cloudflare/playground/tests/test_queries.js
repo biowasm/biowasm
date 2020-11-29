ea763e510f6d5634c17e35c27ccf24598ba8091a
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-11-28 17:29:32 -0800

    Use mocha with some simple validation

--- cloudflare/playground/tests/test_queries.js
+++ cloudflare/playground/tests/test_queries.js
@@ -1,44 +1,89 @@
+const assert = require("assert");
 const webdriver = require("selenium-webdriver");
-const driver = new webdriver.Builder().forBrowser("chrome").build();
 const by = webdriver.By;
 const until = webdriver.until;
 
-// Run all tests
-async function run()
-{
-    await driver.get("http://localhost:5000");
-
-    // Test out each tool (e.g. bedtools, samtools)
-    let tools = await driver.findElements(by.css(".jumbotron button"));
-    for(tool of tools)
-    {
-        // Choose tool and click on Examples dropdown
-        await driver.sleep(200);
-        await tool.click();
-        await driver.sleep(200);
-        await showExamples();
-
-        // Try all the examples for the current tool
-        let examples = await driver.findElements(by.className("dropdown-item"));
-        for(example of examples)
-        {
-            await driver.sleep(200);
-            await example.click()
-            await driver.sleep(200);
-            await showExamples();
-        }
-
-        await example.sendKeys(webdriver.Key.ESCAPE);
-    }
-
-    await driver.quit();
-};
+// Tools to test
+const TOOLS = ["samtools", "bedtools"];
 
 // Click "Examples" button and wait till dropdown menu appears
-async function showExamples()
+async function showExamples(driver)
 {
     await driver.findElement(by.xpath("//button[text() = 'Examples']")).click();
     await driver.wait(until.elementLocated(by.className("dropdown-item")));
 }
 
-run();
+// Tests
+describe("play.biowasm.com", () => {
+    let driver;
+    let elTools;
+    let elCommand;
+    let elOutput;
+    let tools = {};
+
+    // Setup
+    before(async() => {
+        // Launch browser
+        driver = new webdriver.Builder().forBrowser("chrome").build();
+        await driver.get("http://localhost:5000");
+
+        // Track elements of interest
+        elTools = await driver.findElements(by.css(".jumbotron button"));
+        elCommand = await driver.findElement(by.css("input"));
+        elOutput = await driver.findElement(by.css("pre"));
+
+        // Make sure we test all the tools listed on the UI
+        for(let tool of elTools) {
+            let toolName = await tool.getText();
+            tools[toolName] = tool;
+            assert(TOOLS.includes(toolName));
+        }
+    });
+
+    // Tear down
+    after(() => {
+        driver.quit();
+    });
+
+    // Test all examples for each tool
+    for(let tool of TOOLS)
+    {
+        it(`Test ${tool} queries`, async () => {
+            // Choose tool and click on Examples dropdown
+            await driver.sleep(200);
+            await tools[tool].click();
+            await driver.sleep(200);
+            await showExamples(driver);
+
+            // Try all the examples for the current tool
+            let examples = await driver.findElements(by.className("dropdown-item"));
+            for(example of examples)
+            {
+                // Run example
+                await driver.sleep(200);
+                await example.click()
+                await driver.sleep(200);
+
+                // Get output
+                let command = await elCommand.getAttribute("value");
+                let output = await elOutput.getText();
+
+                // Basic validation
+                if(command.includes("--help"))
+                    assert(output.includes(tool));
+                else {
+                    if(tool == "bedtools")
+                        assert(output.includes("chr1"));
+                    else if(tool == "samtools")
+                        assert(output.includes("DRR016846") || output.includes("GL000199") || output.includes("777"));
+                    else
+                        assert(false, `Missing tests for ${tool}`);
+                }
+
+                await showExamples(driver);
+            }
+
+            await example.sendKeys(webdriver.Key.ESCAPE);
+        });
+    }
+});
