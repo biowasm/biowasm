e383c232cff38c399fbea562c30e4b48a3dc172f
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-15 13:12:59 -0700

    SSW: Make two versions of the binaries: one with SIMD and one without

--- tools/ssw/compile.sh
+++ tools/ssw/compile.sh
@@ -2,8 +2,19 @@
 
 cd src/src/
 
-# Compile
+# Compile with SIMD
 emmake make ../../build/ssw.html \
     CC=emcc \
     PROG=../../build/ssw.html \
     CFLAGS="$EM_FLAGS -O3 -pipe -msse2 -msimd128"
+
+# Compile without SIMD (use SIMDe to simulate SIMD support)
+make clean
+git clone https://github.com/simd-everywhere/simde.git
+sed -i -e 's|include <emmintrin.h>|include "simde/simde/x86/sse2.h"|g' *.c *.h
+sed -i -e 's|__m|simde__m|g' *.c *.h
+sed -i -e 's|_mm_|simde_mm_|g' *.c *.h
+emmake make ../../build/ssw-nosimd.html \
+    CC=emcc \
+    PROG=../../build/ssw-nosimd.html \
+    CFLAGS="$EM_FLAGS -O3 -pipe"
