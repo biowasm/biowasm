ce3d4f9f797e9288f2c16ef628a33896888976ff
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-19 20:47:50 -0700

    Cleanup

--- config/shared.cloudflare.sh
+++ config/shared.cloudflare.sh
@@ -1,8 +1,8 @@
-# Commonly used emcc compilation variables for Cloudflare Workers
+# Commonly used Emscripten compilation settings for WebAssembly modules that run on Cloudflare Workers
+# Flags from https://github.com/cloudflare/worker-emscripten-template
 
 . ./config/shared.default.sh
 
-# Flags from https://github.com/cloudflare/worker-emscripten-template
 EM_FLAGS=$(cat <<EOF
     ${EM_FLAGS}
     -s DYNAMIC_EXECUTION=0
@@ -10,13 +10,11 @@ EM_FLAGS=$(cat <<EOF
     -s MODULARIZE=1
     -s ENVIRONMENT="web"
     -s EXPORT_NAME="emscripten"
-    --pre-js ../../../config/pre.cloudflare.js
+    --pre-js ../../../config/shared.cloudflare.js
 EOF
 )
 
 # Remove extraneous whitespace
 EM_FLAGS=$(echo $EM_FLAGS)
-EM_FLAGS_THREADS=$(echo $EM_FLAGS_THREADS)
 
 export EM_FLAGS;
-export EM_FLAGS_THREADS;
--- config/shared.default.sh
+++ config/shared.default.sh
@@ -1,4 +1,4 @@
-# Commonly used emcc compilation variables
+# Commonly used Emscripten compilation settings for WebAssembly modules that run in the browser
 
 EM_FLAGS_BASE=$(cat <<EOF
     -s USE_ZLIB=1
@@ -15,19 +15,7 @@ EM_FLAGS=$(cat <<EOF
 EOF
 )
 
-# Note: avoid using memory growth with threads (see https://emscripten.org/docs/porting/pthreads.html)
-EM_FLAGS_THREADS=$(cat <<EOF
-    ${EM_FLAGS_BASE}
-    -s USE_PTHREADS=1
-    -s PTHREAD_POOL_SIZE=10
-    -s TOTAL_MEMORY=100MB
-    --threadprofiler
-EOF
-)
-
 # Remove extraneous whitespace
 EM_FLAGS=$(echo $EM_FLAGS)
-EM_FLAGS_THREADS=$(echo $EM_FLAGS_THREADS)
 
 export EM_FLAGS;
-export EM_FLAGS_THREADS;
