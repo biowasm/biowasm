4ad89ec8663f625d9d878fa1ff023b97da19b9de
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-21 13:55:09 -0700

    When compiling to WebAssembly, output .js/.wasm without .html

--- tools/fastp/compile.sh
+++ tools/fastp/compile.sh
@@ -3,5 +3,5 @@
 cd src/
 curl -o "testdata/NA12878.fastq.gz" "https://sandbox.bio/data/NA12878.30k.fastq.gz"
 emmake make \
-    TARGET="../build/fastp.html" \
+    TARGET="../build/fastp.js" \
     LIBS="-s USE_ZLIB=1 $EM_FLAGS --preload-file testdata@/fastp/testdata -s ASSERTIONS=1"
--- tools/ssw/compile.sh
+++ tools/ssw/compile.sh
@@ -3,9 +3,9 @@
 cd src/src/
 
 # Compile with SIMD
-emmake make ../../build/ssw.html \
+emmake make ../../build/ssw.js \
     CC=emcc \
-    PROG=../../build/ssw.html \
+    PROG=../../build/ssw.js \
     CFLAGS="$EM_FLAGS -O3 -pipe -msse2 -msimd128"
 
 # Compile without SIMD (use SIMDe to simulate SIMD support)
@@ -14,7 +14,7 @@ git clone https://github.com/simd-everywhere/simde.git
 sed -i -e 's|include <emmintrin.h>|include "simde/simde/x86/sse2.h"|g' *.c *.h
 sed -i -e 's|__m|simde__m|g' *.c *.h
 sed -i -e 's|_mm_|simde_mm_|g' *.c *.h
-emmake make ../../build/ssw-nosimd.html \
+emmake make ../../build/ssw-nosimd.js \
     CC=emcc \
-    PROG=../../build/ssw-nosimd.html \
+    PROG=../../build/ssw-nosimd.js \
     CFLAGS="$EM_FLAGS -O3 -pipe"
