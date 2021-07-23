fb9f1d9b5b2ac60147eb8e046b9123132f7ca35a
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-23 08:04:21 -0700

    Update stats: On a cron, fetch new raw events, aggregate them, delete them, then create summary aggregated data

--- cloudflare/stats/index.js
+++ cloudflare/stats/index.js
@@ -1,37 +1,34 @@
 // This file defines a Cloudflare Worker that:
 //   (1) When called as a cron job, tallies up how often each tool was downloaded
-//       from the CDN over time, and stores those counts as "SUMMARY_AGGREGATED".
+//       from the CDN over time, and stores those counts as "summary".
 //   (2) When called as a GET endpoint, uses the summary stats calculated by the
 //       cron above to plot a chart of download counts over time.
 
-// KV key names; summaries are calculated and split by versions,
-// or aggregateda cross all versions.
-const KEY_SUMMARY_SPLIT = "SUMMARY_SPLIT";
-const KEY_SUMMARY_AGGREGATED = "SUMMARY_AGGREGATED";
+// KV key name
+const KEY_SUMMARY = "summary";
 
 // ================================================================
 // Web endpoint
 // ================================================================
 async function handleRequest(request)
 {
-  let data = [];
-  let stats = JSON.parse(await LOGS.get(KEY_SUMMARY_AGGREGATED));
+	let data = [];
+	let stats = JSON.parse(await LOGS.get(KEY_SUMMARY));
 
-  // Convert to Plotly format
-  // Format: { "seqtk": { "2020-01-01": 4 } }
-  for(let prgm in stats)
-  {
-    let x = Object.keys(stats[prgm]).sort();
-    let y = x.map(d => stats[prgm][d]);
-    data.push({
-      x: x,
-      y: y,
-      name: prgm,
-      type: "scatter"
-    });
-  }
+	// Convert to Plotly format
+	// KV format: { "date": "tool": count }
+	// Plotly format: [ { name: "tool", type: "scatter", x: [date1, date2, ...], y: [count1, count2, ...] } ]
+	for(let prgm in stats) {
+		let x = Object.keys(stats[prgm]).sort();
+		let y = x.map(d => stats[prgm][d]);
+		data.push({
+			name: prgm,
+			x: x, y: y,
+			type: "scatter"
+		});
+	}
 
-  return new Response(`
+	return new Response(`
         <!doctype html>
         <html lang="en">
           <head>
@@ -98,9 +95,7 @@ async function handleRequest(request)
             </main>
           </body>
         </html>
-    `, {
-    headers: { "content-type": "text/html;charset=UTF-8" }
-  });
+    `, { headers: { "content-type": "text/html;charset=UTF-8" } });
 }
 
 // ================================================================
@@ -108,62 +103,113 @@ async function handleRequest(request)
 // ================================================================
 async function handleSchedule(scheduledDate)
 {
-  console.log(scheduledDate);
+	console.log(scheduledDate);
+	// ========================================================================
+	// First, look for raw, unaggregated log events, and aggregate them
+	// Raw events: raw:<date>:<tool>/<version>/<program>.wasm:<uuid> value="" metadata={}
+	// ========================================================================
+	let aggregated = {};  // { "2021-07-22": { "bowtie2": 123, ... }, ... }
+	let rawKeys = await getLogs({
+		prefix: "raw:",
+		fn: key => {
+			const value = key.name.split(":");         // key=raw:2021-07-22:samtools:uuid
+			const date = value[1];                     // 2021-07-22
+			const tool = value[2];                     // samtools
 
-  let statsSplit = {};       // {"/aioli/1.3.0/aioli.js": { "2020-01-01": 4 }}
-  let statsAggregated = {};  // {"aioli": { "2020-01-01": 4 }}
+			// Aggregate all versions
+			if(!(date in aggregated))
+				aggregated[date] = {};
+			if(!(tool in aggregated[date]))
+				aggregated[date][tool] = 0;
+			aggregated[date][tool]++;
 
-  // Loop through all key names using "cursor" for pagination.
-  // Note that the .list() API doesn't return values so we do
-  // that separately.
-  let response = { cursor: null };
-  do {
-    // Get next page of keys
-    response = await LOGS.list({
-      cursor: response.cursor
-    });
+			return key.name;
+		}
+	});
+	console.log(rawKeys)
+	console.log(aggregated)
 
-    // Parse the metadata (i.e. download counts) for every key
-    // Format:
-    // { keys: [{ name: "name", metadata: 123}], 
-    //   list_complete: false, cursor: "<CURSOR ID>"}] }
-    response.keys.map(d => {
-      const key = d.name.replace("/v2", "");
-      const count = d.metadata;
+	// Now that they're aggregated, we can update the key/value store counts
+	for(let date in aggregated) {
+		for(let tool in aggregated[date]) {
+			const key = `aggregate:${date}:${tool}`;
+			const count = parseInt((await LOGS.getWithMetadata(key)).metadata || 0);
+			const countToAdd = aggregated[date][tool];
+			await LOGS.put(key, "", { metadata: count + countToAdd });
+		}
+	}
 
-      // Parse key
-      [date, path] = key.split("|");
-      if(path == null)
-        return;
-      [_, prgm] = path.split("/");
+	// And we can delete the raw log events
+	for(let key of rawKeys) {
+		console.log(`Deleting ${key}...`)
+		await LOGS.delete(key);
+	}
 
-      // Split by version
-      if(!(path in statsSplit))
-        statsSplit[path] = {};
-      if(!(date in statsSplit[path]))
-        statsSplit[path][date] = 0;
-      statsSplit[path][date] += count;
-      // Aggregate all versions
-      if(!(prgm in statsAggregated))
-        statsAggregated[prgm] = {};
-      if(!(date in statsAggregated[prgm]))
-        statsAggregated[prgm][date] = 0;
-      statsAggregated[prgm][date] += count;
-    });
-  } while(response.cursor != null)
+	// ========================================================================
+	// Now fetch *all* aggregate KVs and save that to a summary KV
+	// Aggregate events: aggregate:<date>:<prgm>  value="" metadata=count
+	// Note that we store the summary JSON as { "tool": { "date" } } so it's 
+	// easier to plot as different series, but we store the key/value pairs as
+	// "date:tool" for sorting purposes.
+	// ========================================================================
+	aggregated = {};
+	await getLogs({
+		prefix: "aggregate:",
+		fn: key => {
+			const value = key.name.split(":");  // key=aggregate:2021-07-22:samtools metadata=count
+			const date = value[1];              // e.g. 2021-07-22
+			const prgm = value[2];              // e.g. samtools
+			const count = key.metadata;         // e.g. 123
 
-  await LOGS.put(KEY_SUMMARY_SPLIT, JSON.stringify(statsSplit));
-  await LOGS.put(KEY_SUMMARY_AGGREGATED, JSON.stringify(statsAggregated));
+			// Aggregate all versions
+			if(!(prgm in aggregated))
+				aggregated[prgm] = {};
+			if(!(date in aggregated[prgm]))
+				aggregated[prgm][date] = 0;
+			aggregated[prgm][date] += count;
+		}
+	});
+	console.log(aggregated);
+
+	// Save it so we can fetch it later quickly from the stats page
+	await LOGS.put(KEY_SUMMARY, JSON.stringify(aggregated));
 }
+
+
+// ================================================================
+// Utility functions
+// ================================================================
+
+async function getLogs(config)
+{
+	// Loop through all keys and handle pagination
+	let processed = [];
+	let response = { cursor: null };
+	do {
+		response = await LOGS.list({
+			prefix: config.prefix,
+			cursor: response.cursor
+		});
+
+		processed = processed.concat(response.keys.map(key => {
+			if(!config.fn)
+				return key
+			return config.fn(key);
+		}))
+	} while(response.cursor != null);
+
+	return processed;
+}
+
 
 // ================================================================
 // Event listeners
 // ================================================================
 
 addEventListener("fetch", event => {
-  return event.respondWith(handleRequest(event.request));
+	return event.respondWith(handleRequest(event.request));
 });
 
 addEventListener("scheduled", event => {
-  event.waitUntil(handleSchedule(event.scheduledTime));
+	event.waitUntil(handleSchedule(event.scheduledTime));
 });
