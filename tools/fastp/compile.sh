141c4830ec44d5b298f48b9aea1532be22b6975e
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-23 20:28:50 -0700

    Add sample file to fastp

--- tools/fastp/compile.sh
+++ tools/fastp/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd src/
-curl -o "testdata/NA12878.fastq.gz" "https://sandbox.bio/data/NA12878.30k.fastq.gz"
+cp ../data/NA12878.fastq.gz testdata/
 emmake make \
     TARGET="../build/fastp.js" \
     LIBS="-s USE_ZLIB=1 $EM_FLAGS --preload-file testdata@/fastp/testdata -s ASSERTIONS=1"
