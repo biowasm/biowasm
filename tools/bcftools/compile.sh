8348c2a3310033dee434b1e7a900c92391392621
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-03 13:58:38 -0700

    Add bcftools compilation script and patch

---
+++ tools/bcftools/compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd src/
+
+# Configure
+autoheader
+autoconf
+emconfigure ./configure --with-htslib="../../htslib/src/" 
+
+# Compile to WebAssembly
+emmake make bcftools CC=emcc AR=emar \
+    CFLAGS="-O2 -s USE_ZLIB=1 -s USE_BZIP2=1" \
+    LDFLAGS="$EM_FLAGS -s ERROR_ON_UNDEFINED_SYMBOLS=0 -O2 --preload-file test/annotate.vcf@/bcftools/annotate.vcf"
