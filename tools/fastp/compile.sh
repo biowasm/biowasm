fe873f24713dc2ccf514231478932419d2fae51b
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-11 13:05:53 -0700

    fastp: optimize .js glue code size

--- tools/fastp/compile.sh
+++ tools/fastp/compile.sh
@@ -4,4 +4,4 @@ cd src/
 cp ../data/NA12878.fastq.gz testdata/
 emmake make \
     TARGET="../build/fastp.js" \
-    LIBS="-s USE_ZLIB=1 $EM_FLAGS --preload-file testdata@/fastp/testdata -s ASSERTIONS=1"
+    LIBS="-O3 -s USE_ZLIB=1 $EM_FLAGS --preload-file testdata@/fastp/testdata -s ASSERTIONS=1"
