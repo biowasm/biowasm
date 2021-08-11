fb56ccae1887bac35595f2265d33e045aabfd583
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-11 13:17:33 -0700

    bedtools: optimize js glue code

--- tools/bedtools/compile.sh
+++ tools/bedtools/compile.sh
@@ -14,4 +14,4 @@ emmake make \
     BIN_DIR="../build/" \
     CXX="em++ -s USE_ZLIB=1 -s USE_BZIP2=1" \
     BT_LIBS="-s USE_ZLIB=1 -s USE_BZIP2=1 -lm -lpthread" \
-    BT_LDFLAGS="--preload-file test@/bedtools/test $EM_FLAGS"
+    BT_LDFLAGS="-O2 --preload-file test@/bedtools/test $EM_FLAGS"
