d91d7181e8993b5c66ae949fb3d44a3c385c4d9d
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-16 08:10:55 -0700

    Cleanup

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,8 @@
 DIR_TOOLS="config/tools.json"
 DIR_CDN="cloudflare/cdn/public"
 URL_CDN="https://cdn.biowasm.com/v2"
+# ENV               # Either "stg" or "prd"
+# TOOLS_TO_COMPILE  # Comma-separated list of tools to recompile; the rest will use CDN as cache ('all'=all tools, ''=none)
 
 # ------------------------------------------------------------------------------
 # Setup repos and dependencies
