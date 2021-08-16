167478054106223781dfe97509ddb50d64a2f936
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-08-16 07:54:31 -0700

    Checkpoint

--- tools/minimap2/compile.sh
+++ tools/minimap2/compile.sh
@@ -2,15 +2,15 @@ cd src/
 
 WASM_FLAGS="$EM_FLAGS --preload-file test/@/minimap2/"
 
-# Non-SIMD
-echo "Compiling without SIMD"
-make clean
-emmake make \
-	-f Makefile.simde \
-	PROGRAM="minimap2-nosimd" \
-	CFLAGS="-O2 -msimd128 -Wno-pass-failed" \
-	WASM_FLAGS="$WASM_FLAGS" \
-	minimap2-nosimd
+# # Non-SIMD
+# echo "Compiling without SIMD"
+# make clean
+# emmake make \
+# 	-f Makefile.simde \
+# 	PROGRAM="minimap2-nosimd" \
+# 	CFLAGS="-O2 -msimd128 -Wno-pass-failed" \
+# 	WASM_FLAGS="$WASM_FLAGS" \
+# 	minimap2-nosimd
 
 # SIMD
 echo "Compiling with SIMD"
@@ -20,6 +20,9 @@ emmake make \
 	PROGRAM="minimap2" \
 	sse2only=1 \
 	CFLAGS="-O2 -msimd128" \
-	WASM_FLAGS="$WASM_FLAGS" \
+	WASM_FLAGS="$WASM_FLAGS -s ERROR_ON_UNDEFINED_SYMBOLS=0" \
 	minimap2
 
+exit
+
+minimap2 -v 100 -a /minimap2/MT-human.fa /minimap2/MT-orang.fa > test.sam
