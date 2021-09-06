a4bf0e991857948c658e84dc228c3077066c4163
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-09-06 14:49:01 -0700

    Add jq compilation script

---
+++ tools/jq/compile.sh
@@ -0,0 +1,14 @@
+cd src/
+
+# Dependencies
+apt-get install -y libtool
+
+# Generate and run ./configure
+autoreconf -fi
+emconfigure ./configure \
+    --with-oniguruma=builtin \
+    --disable-maintainer-mode
+
+# Build
+emmake make EXEEXT=.js CFLAGS="-O2 $EM_FLAGS"
+mv jq.{js,wasm} ../build/
