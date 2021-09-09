da6989009bab22091e1b65be119f35498aa8c186
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-09-09 07:58:53 -0700

    Checkpoint

--- tools/minimap2/compile.sh
+++ tools/minimap2/compile.sh
@@ -1,16 +1,18 @@
 cd src/
 
+# . ../../../config/shared.default.sh
+
 WASM_FLAGS="$EM_FLAGS --preload-file test/@/minimap2/"
 
 # # Non-SIMD
 # echo "Compiling without SIMD"
 # make clean
-# emmake make \
-# 	-f Makefile.simde \
-# 	PROGRAM="minimap2-nosimd" \
-# 	CFLAGS="-O2 -msimd128 -Wno-pass-failed" \
-# 	WASM_FLAGS="$WASM_FLAGS" \
-# 	minimap2-nosimd
+emmake make \
+	-f Makefile.simde \
+	PROGRAM="minimap2-nosimd" \
+	CFLAGS="-O2 -msimd128 -Wno-pass-failed" \
+	WASM_FLAGS="$WASM_FLAGS" \
+	minimap2-nosimd
 
 # SIMD
 echo "Compiling with SIMD"
@@ -19,10 +21,16 @@ emmake make \
 	-f Makefile \
 	PROGRAM="minimap2" \
 	sse2only=1 \
-	CFLAGS="-O2 -msimd128" \
-	WASM_FLAGS="$WASM_FLAGS -s ERROR_ON_UNDEFINED_SYMBOLS=0" \
+	CFLAGS="-g -O2 -msimd128" \
+	WASM_FLAGS="$WASM_FLAGS -s ASSERTIONS=1" \
 	minimap2
+
+# 
+
 
 exit
 
-minimap2 -v 100 -a /minimap2/MT-human.fa /minimap2/MT-orang.fa > test.sam
+time minimap2 -a /minimap2/MT-human.fa /minimap2/MT-orang.fa > test.sam
+time minimap2 -x map-ont -d MT-human-ont.mmi /minimap2/MT-human.fa
+time minimap2 -a MT-human-ont.mmi /minimap2/MT-orang.fa > test.sam
+
