e2f2e0450ec7917271ff5a2ab126feeea9d82a5a
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-10-04 11:51:48 -0700

    Add script to compile gawk to WebAssembly

--- tools/gawk/compile.sh
+++ tools/gawk/compile.sh
@@ -1,1 +1,17 @@
 # TODO
+cd src/
+
+# Disable NLS to avoid memory out of bounds error (https://github.com/emscripten-core/emscripten/issues/11621#issuecomment-691807912)
+emconfigure ./configure \
+  --disable-nls \
+  --disable-extensions \
+  --disable-largefile
+
+cd support && emmake make && cd ..
+
+# Compile gawk to WebAssembly
+emmake make \
+	EXEEXT=.js \
+	LIBS="-s ERROR_ON_UNDEFINED_SYMBOLS=0 $EM_FLAGS" \
+	CFLAGS="-O2 -DNDEBUG" \
+	AM_CFLAGS="" gawk.js
