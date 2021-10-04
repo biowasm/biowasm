d3cd6486851af8656d90f153780c5a107eb0edd6
Chris Wright <chris.wright@nanoporetech.com>
2021-10-04 01:03:51 +0100

    update modbam2bed branch

--- tools/modbam2bed/compile.sh
+++ tools/modbam2bed/compile.sh
@@ -13,11 +13,8 @@ cd htslib
 make clean
 autoheader
 autoconf
-emconfigure ./configure CFLAGS="-s USE_ZLIB=1 -s USE_BZIP2=1" --disable-lzma
-
-# get a lot of "undefined symbol: pthread_attr_getstacksize" here
-emmake make libhts.a CC="emcc" AR=emar \
-    LDFLAGS="-s ERROR_ON_UNDEFINED_SYMBOLS=0 -O2"
+emconfigure ./configure CFLAGS="-s USE_ZLIB=1 -s USE_BZIP2=1 -O2" --disable-lzma  --disable-libcurl
+emmake make libhts.a CC="emcc" AR=emar
 cd ..
 
 
@@ -27,15 +24,24 @@ tar -xzvf argp-standalone-1.3.tar.gz
 cd argp-standalone-1.3
 wget https://raw.githubusercontent.com/Homebrew/formula-patches/b5f0ad3/argp-standalone/patch-argp-fmtstream.h
 patch argp-fmtstream.h patch-argp-fmtstream.h
-emconfigure ./configure
+emconfigure ./configure 
 emmake make CC=emcc AR=emar \
     LDFLAGS="$EM_FLAGS -O2"
 cd ..
 
 
-## build program
+# build program
+# The define `WASM` disables posix resource limit checks
+# The define NOTHREADS disables use of hts_thread_pool which leads to error pthread_attr_getstacksize
+# being missing when compilation is not performed without `-pthread`. The use of `-pthread` triggers
+# warnings with emcc that pthreads + MODULARIZE requires setting EXPORT_NAME != Module. However this
+# doesnt appear compatible with aioli. Consequently we need to set ERROR_ON_UNDEFINED_SYMBOLS=0 to
+# ignore the undefined pthread_attr_getstacksize function.
 emmake make modbam2bed CC=emcc AR=emar \
-    CFLAGS="-s EXPORT_NAME=modbamextract -s USE_ZLIB=1 -s USE_BZIP2=1 -fno-stack-protector -O2" \
+    CFLAGS="-DNOTHREADS -DWASM -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s USE_ZLIB=1 -s USE_BZIP2=1 -fno-stack-protector -O2" \
     ARGP=argp-standalone-1.3/libargp.a \
     EXTRA_CFLAGS="-Iargp-standalone-1.3" \
     EXTRA_LDFLAGS="$EM_FLAGS"
+cd ..
+cp src/modbam2bed build/modbam2bed.js
+cp src/modbam2bed.wasm build
