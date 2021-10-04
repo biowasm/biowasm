1ecd8b2599dbc1753957360870d3385dce5cbf04
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-10-04 12:01:25 -0700

    Cleanup

--- tools/gawk/compile.sh
+++ tools/gawk/compile.sh
@@ -1,15 +1,14 @@
-# TODO
 cd src/
 
 # Disable NLS to avoid memory out of bounds error (https://github.com/emscripten-core/emscripten/issues/11621#issuecomment-691807912)
 emconfigure ./configure \
-  --disable-nls \
-  --disable-extensions \
-  --disable-largefile
+	--disable-nls \
+	--disable-extensions \
+	--disable-largefile
 
-cd support && emmake make && cd ..
 
 # Compile gawk to WebAssembly
+cd support && emmake make && cd ..
 emmake make \
 	EXEEXT=.js \
 	LIBS="-s ERROR_ON_UNDEFINED_SYMBOLS=0 $EM_FLAGS" \
