fbcb64c54e178899cca90874847c9a28a5e8f3ef
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-11-09 11:04:56 -0800

    Add sample Cloudflare Worker script that runs a biowasm module

--- config/shared.cloudflare.js
+++ config/shared.cloudflare.js
@@ -1,8 +1,33 @@
-// Emscripten pre.js file for Cloudflare Workers environment
-const document = this
+// Cloudflare Worker Script.
+// Must specify `wasm_modules = { WASM_MODULE = "./wgsim.wasm" }` in wrangler.toml.
+// See <https://github.com/robertaboukhalil/cf-workers-emscripten/> for an example.
 
-// A way to retrieve stdout/stderr
-Module.OUT = { stdout: "", stderr: "" };
-Module.print = (function(d) { Module.OUT.stdout += `${d}\n` });
-Module.printErr = (function(d) { Module.OUT.stderr += `${d}\n` });
-Module.getOutput = (function(d) { return Module.OUT });
+import Module from "wgsim.js";
+
+addEventListener("fetch", event => {
+	event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+	// Initialize WebAssembly module
+	let output = "";
+	const m = await Module({
+		// By default, stdout/stderr is output to console.log/warn
+		print: text => output += `${text}\n`,
+		printErr: text => output += `${text}\n`,
+
+		// Instead of downloading the .wasm file, fetch it from a global var
+		instantiateWasm: (imports, callback) => {
+			// Note that "PI_WASM" is defined in wrangler.toml
+			const instance = new WebAssembly.Instance(PI_WASM, imports);
+			callback(instance);
+			return instance.exports;
+		}
+	});
+
+	// Call main function
+	m.callMain([ "--help" ]);
+
+	// Return output
+	return new Response(output);
+}
