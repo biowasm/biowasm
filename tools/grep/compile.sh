d2f5eca2c6920f6b2a3e25f41f8eeb7cc0d5844d
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-01-22 18:58:05 -0800

    Add compile script

---
+++ tools/grep/compile.sh
@@ -0,0 +1,17 @@
+cd src/
+
+# Install dependencies
+apt-get install -y pkg-config
+./bootstrap
+
+# Nanosleep not supported in Emscripten
+sed -i 's|if ${gl_cv_func_sleep_works+:} false|if true|g' configure
+sed -i 's|if ${ac_cv_search_nanosleep+:} false|if true|g' configure
+sed -i 's|if ${gl_cv_func_nanosleep+:} false|if true|g' configure
+
+# Configure
+emconfigure ./configure --disable-nls
+
+# Run Makefile on each subfolder. Don't error on undefined _splice function that isn't used
+emmake make all CC=emcc -k WERROR_CFLAGS="" CFLAGS="$EM_FLAGS -O2 -s ERROR_ON_UNDEFINED_SYMBOLS=0" EXEEXT=.js
+mv src/grep.{js,wasm} ../build/
