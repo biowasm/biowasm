f92fc2dd3ccdafc44e8883594fb7dd0deeafa436
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-05-17 20:28:42 -0700

    Add code for compiling tabix/htsfile/bgzip

--- tools/htslib/compile.sh
+++ tools/htslib/compile.sh
@@ -11,3 +11,11 @@ make clean
 autoheader
 autoconf
 emconfigure ./configure CFLAGS="-s USE_ZLIB=1 -s USE_BZIP2=1" --disable-lzma
+
+# Build
+TOOLS=("tabix" "htsfile" "bgzip")
+for tool in ${TOOLS[@]}; do
+    emmake make $tool CC=emcc AR=emar \
+        CFLAGS="-O2 -s USE_ZLIB=1 -s USE_BZIP2=1" \
+        LDFLAGS="$EM_FLAGS -O2"
+done
