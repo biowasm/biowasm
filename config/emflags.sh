b4297d77794ca425c171216da527fb83e7eb7dd0
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-06-20 08:51:17 -0700

    Simplify $EM_FLAGS

--- config/emflags.sh
+++ config/emflags.sh
@@ -1,23 +1,17 @@
 # Commonly used Emscripten compilation settings for WebAssembly modules that run in the browser
-
-EM_FLAGS_BASE=$(cat <<EOF
+EM_FLAGS=$(cat <<EOF
     -s USE_ZLIB=1
     -s INVOKE_RUN=0
     -s FORCE_FILESYSTEM=1
     -s EXPORTED_RUNTIME_METHODS=["callMain","FS","PROXYFS","WORKERFS"]
     -s MODULARIZE=1
     -s ENVIRONMENT="web,worker"
+    -s ALLOW_MEMORY_GROWTH=1
     -lworkerfs.js -lproxyfs.js
 EOF
 )
-
-EM_FLAGS=$(cat <<EOF
-    ${EM_FLAGS_BASE}
-    -s ALLOW_MEMORY_GROWTH=1
-EOF
-)
-
-# Remove extraneous whitespace
+# Remove whitespace
 EM_FLAGS=$(echo $EM_FLAGS)
 
+# Export
 export EM_FLAGS;
