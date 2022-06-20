3ed6055903432f28850d8b1f83b59a5244f2b884
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-06-20 10:22:54 -0700

    Add sed

---
+++ tools/sed/compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd src/
+
+# Install dependencies
+sudo apt-get install -y pkg-config autopoint gperf help2man gettext texinfo bison
+./bootstrap
+EM_GNU_NANOSLEEP
+
+# Configure
+emconfigure ./configure --disable-nls
+
+# Build
+emmake make sed CC=emcc -k WERROR_CFLAGS="" CFLAGS="$EM_FLAGS -O2 -s ERROR_ON_UNDEFINED_SYMBOLS=0" EXEEXT=.js
+mv sed/sed.{js,wasm} ../build/
