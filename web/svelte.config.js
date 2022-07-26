20c72e7794929fbd2a4ce8102d0d59a55d04e6f1
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-07-26 16:33:58 -0700

    Cleanup

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from "@sveltejs/adapter-cloudflare-workers";
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
--- web/vite.config.js
+++ web/vite.config.js
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
-/** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()]
 };
