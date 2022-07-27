b589f9af2f73aab2da89597e3ec43724f14dcdd1
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-07-27 15:50:25 -0700

    Support package dependencies in compile script

--- bin/compile.py
+++ bin/compile.py
@@ -5,25 +5,44 @@ import argparse
 
 # State
 CONFIG = {}
+DIR_BUILD = "build"
 
 
 def list():
 	"""
-	
+	List all biowasm packages
 	"""
-	print(CONFIG)
+	for tool in CONFIG["tools"]:
+		name = tool["name"]
+		versions = ", ".join([ v["version"] for v in tool["versions"] ])
+		print(f"{name.ljust(10)}\t{versions}")
 
 
-def compile(tools, versions=None):
+def compile(tool, versions=[]):
 	"""
+	Compile one tool, multiple versions
 	"""
-	for tool in tools:
-		tool = next(t for t in CONFIG["tools"] if t["name"] == tool)
-		print(f"make init {tool['name']}")
-		if versions != ['']:
-			tool["versions"] = [ v for v in tool["versions"] if v["version"] in versions ]
-		for version in tool["versions"]:
-			print(f"tools/compile.sh tools/{tool['name']} {version['branch']}")
+	# print(f"# Compiling '{tool}' versions '{versions}'")
+	tool_info = next(t for t in CONFIG["tools"] if t["name"] == tool)
+	files = tool_info["files"] if "files" in tool_info else ["js", "wasm"]
+	programs = tool_info["programs"] if "programs" in tool_info else [tool]
+	versions = [v for v in tool_info["versions"] if v["version"] in versions] if versions else tool_info["versions"]
+
+	print(f"TOOL={tool} make init")
+
+	for version_info in versions:
+		version = version_info["version"]
+		branch = version_info["branch"]
+		dependencies = version_info["dependencies"] if "dependencies" in version_info else []
+		for dependency_info in dependencies:
+			compile(dependency_info["name"], [dependency_info["version"]])
+
+		dir_build = f"{DIR_BUILD}/{tool}/{version}"
+		print(f"mkdir -p {dir_build}")
+		print(f"bin/compile.sh tools/{tool} {branch}")
+		for program in programs:
+			for file in files:
+				print(f"cp tools/{tool}/build/{program}.{file} {dir_build}/")
 
 
 if __name__ == "__main__":
@@ -36,12 +55,13 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--list", default=False, action="store_true", help="List tools available")
 	parser.add_argument("--tools", type=str, help="Tool to compile to WebAssembly")
-	parser.add_argument("--versions", default="", type=str, help="Tool version(s)")
+	parser.add_argument("--versions", type=str, help="Tool version(s)")
 	args = parser.parse_args()
 
 	if args.list:
 		list()
 	elif args.tools:
-		compile(args.tools.split(","), args.versions.split(","))
+		for tool_name in args.tools.split(","):
+			compile(tool_name, args.versions.split(",") if args.versions is not None else [])
 	else:
 		parser.print_usage()
