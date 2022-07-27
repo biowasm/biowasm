d40e7fae06e138bf6d16e115e441ca92e7b5df15
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-07-27 11:59:13 -0700

    Initial logic for compile script

---
+++ tools/compile.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3.8
+
+import json
+import argparse
+
+# State
+CONFIG = {}
+
+
+def list():
+	"""
+	
+	"""
+	print(CONFIG)
+
+
+def compile(tools, versions=None):
+	"""
+	"""
+	for tool in tools:
+		tool = next(t for t in CONFIG["tools"] if t["name"] == tool)
+		if versions != ['']:
+			tool["versions"] = [ v for v in tool["versions"] if v["version"] in versions ]
+		for version in tool["versions"]:
+			print(f"tools/compile.sh tools/{tool['name']} {version['branch']}")
+
+
+if __name__ == "__main__":
+	"""
+	Parse user arguments
+	"""
+	with open("biowasm.json") as f:
+		CONFIG = json.load(f)
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--list", default=False, action="store_true", help="List tools available")
+	parser.add_argument("--tools", type=str, help="Tool to compile to WebAssembly")
+	parser.add_argument("--versions", default="", type=str, help="Tool version(s)")
+	args = parser.parse_args()
+
+	if args.list:
+		list()
+	elif args.tools:
+		compile(args.tools.split(","), args.versions.split(","))
+	else:
+		parser.print_usage()
