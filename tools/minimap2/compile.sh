9d0675f25a1bb85cc8841782304dda9a84aab3bc
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-07-29 05:16:37 -0700

    Add -simd to SIMD-enabled tools to match Aioli v3 change

--- tools/minimap2/compile.sh
+++ tools/minimap2/compile.sh
@@ -7,17 +7,17 @@ echo "Compiling without SIMD"
 make clean
 emmake make \
 	-f Makefile.simde \
-	PROGRAM="minimap2-nosimd" \
+	PROGRAM="minimap2" \
 	CFLAGS="-O2 -Wno-pass-failed -Wno-return-type" \
 	WASM_FLAGS="$WASM_FLAGS" \
-	minimap2-nosimd
+	minimap2
 
 # SIMD
 echo "Compiling with SIMD"
 make clean
 emmake make \
 	-f Makefile \
-	PROGRAM="minimap2" \
+	PROGRAM="minimap2-simd" \
 	CFLAGS="-O2 -Wno-return-type -Wno-unused-command-line-argument -msimd128" \
 	WASM_FLAGS="$WASM_FLAGS" \
-	minimap2
+	minimap2-simd
--- tools/ssw/compile.sh
+++ tools/ssw/compile.sh
@@ -2,9 +2,9 @@
 
 # Compile with SIMD
 cd src/
-emmake make ../../build/ssw.js \
+emmake make ../../build/ssw-simd.js \
     CC=emcc \
-    PROG=../../build/ssw.js \
+    PROG=../../build/ssw-simd.js \
     CFLAGS="$EM_FLAGS -O3 -pipe -msse2 -msimd128"
 
 # Compile without SIMD (use SIMDe to simulate SIMD support)
@@ -13,7 +13,7 @@ git clone https://github.com/simd-everywhere/simde.git
 sed -i -e 's|include <emmintrin.h>|include "simde/simde/x86/sse2.h"|g' *.c *.h
 sed -i -e 's|__m|simde__m|g' *.c *.h
 sed -i -e 's|_mm_|simde_mm_|g' *.c *.h
-emmake make ../../build/ssw-nosimd.js \
+emmake make ../../build/ssw.js \
     CC=emcc \
-    PROG=../../build/ssw-nosimd.js \
+    PROG=../../build/ssw.js \
     CFLAGS="$EM_FLAGS -O3 -pipe"
