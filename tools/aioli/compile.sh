2adc589c05f7bb95e56b2066ac49cdbb29a397b6
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-07-28 21:28:37 -0700

    Cleanup

--- tools/aioli/compile.sh
+++ tools/aioli/compile.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# # Need at least Node v16
-# # FIXME: move to deploy.yml
-# curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-# sudo apt install -y nodejs
-
-# 
+# Build Aioli
 npm install
 npm run build
 cp dist/aioli.js ../build/
