d616bb0100275768292dc9cea94a91f31d230460
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-07-31 20:23:26 -0700

    For now, get stats for all programs through Durable Objects

--- web/src/routes/api/v3/stats/[...path].js
+++ web/src/routes/api/v3/stats/[...path].js
@@ -1,41 +1,39 @@
-import { isValidTool } from "$lib/utils";
 import CONFIG from "@/biowasm.json";
 
-// TODO: GET /api/v3/stats
-// TODO: GET /api/v3/:tool/stats
-// TODO: GET /api/v3/:tool/:version/stats
+// GET /api/v3/stats
+// GET /api/v3/stats/:tool
+// GET /api/v3/stats/:tool/:version
+// GET /api/v3/stats/:tool/:version/:program
 export async function GET({ request, platform, params }) {
 	let [toolName, version, program] = params.path.split("/", 3);
 	toolName ||= null, version ||= null, program ||= null;
 
-	// TODO: Get all stats from KV (more efficient than querying all the tools?)
-	if(toolName === null) {
-		// console.log({...await platform.env.stats.list()});
-		return {
-			status: 200,
-			body: { stats: "TODO" }
-		};
-	}
+	// // TODO: Get all stats from KV (more efficient than querying all the tools?)
 
 	// Get tool info
-	const tool = CONFIG.tools.find(t => t.name === toolName);
-	if(!tool)
-		return error(params);
-	// Get matching version(s)
-	const versions = tool.versions.filter(v => version === null || v.version === version);
-	if(versions.length === 0)
-		return error(params);
-	// Get matching program(s)
-	const programs = (tool.programs || [toolName]).filter(p => program === null || p === program);
-	if(programs.length === 0)
+	const tools = CONFIG.tools.filter(t => toolName === null || t.name === toolName);
+	if(tools.length === 0)
 		return error(params);
 
 	// Gather stats for each program
 	let stats = {};
-	for(let program of programs) {
-		stats[program] = {};
-		for(let versionInfo of versions)
-			stats[program][versionInfo.version] = await getStats(toolName, versionInfo.version, program);
+	for(let tool of tools) {
+		// Get matching version(s)
+		const versions = tool.versions.filter(v => version === null || v.version === version);
+		if(versions.length === 0)
+			return error(params);
+		// Get matching program(s)
+		const programs = tool.programs.filter(p => program === null || p === program);
+		if(programs.length === 0)
+			return error(params);
+
+		// Update stats
+		stats[tool.name] = {};
+		for(let programName of programs) {
+			stats[tool.name][programName] = {};
+			for(let version of versions)
+				stats[tool.name][programName][version.version] = await getStats(tool.name, version.version, programName);
+		}
 	}
 
 	return {
