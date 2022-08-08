b23cef838d3366ab8741ee4eba85b27981aada44
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-08-08 10:42:01 -0700

    Show dependencies as pills

--- web/src/routes/cdn/v3/[tool]/[version]/index.svelte
+++ web/src/routes/cdn/v3/[tool]/[version]/index.svelte
@@ -1,5 +1,6 @@
 <script context="module">
 import CONFIG from "@/biowasm.json";
+import { Badge, ListGroup, ListGroupItem } from "sveltestrap";
 
 export async function load({ params }) {
 	const tool = CONFIG.tools.find(t => t.name === params.tool);
@@ -23,18 +24,25 @@ export let version;
 
 <base href="{CONFIG.url}/{tool.name}/{version.version}/" />
 
-{#if version.dependencies}
-	Dependencies:<br />
-	{#each version.dependencies as dependency}
-		* <a href="{CONFIG.url}/{dependency.name}/{dependency.version}">{dependency.name} v{dependency.version}</a><br />
-	{/each}
-	<br />
-{/if}
+<!-- Dependencies -->
+{#each version.dependencies || [] as dependency}
+	<a href="{CONFIG.url}/{dependency.name}/{dependency.version}">
+		<Badge pill color="primary" class="mb-4">Depends on {dependency.name} v{dependency.version}</Badge>
+	</a>
+{/each}
 
-Files:<br />
+<!-- Sample Usage -->
+<h5>Sample Usage</h5>
+
+
+<!-- Files to download -->
+<h5>Files</h5>
 {#each tool.programs || [tool.name] as program}
-	{#each tool.files || ["js", "wasm"] as file}
-		<!-- Need full page reload to download the file -->
-		* <a sveltekit:reload href="{program}.{file}">{program}.{file}</a><br />
-	{/each}
+	<h6 class="mt-3">{program}</h6>
+	<ListGroup>
+		{#each tool.files || ["js", "wasm"] as extension}
+			<!-- Need full page reload to download the file -->
+			<ListGroupItem tag="a" href="{program}.{extension}" action>{program}.{extension}</ListGroupItem>
+		{/each}
+	</ListGroup>
 {/each}
