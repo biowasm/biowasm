50e19cac5f0ce6af6b5af1746a8262e78ef0d1a7
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-08-08 11:00:52 -0700

    Show stats on packages page

--- web/src/routes/cdn/v3/[tool]/[version]/index.svelte
+++ web/src/routes/cdn/v3/[tool]/[version]/index.svelte
@@ -18,17 +18,31 @@ export async function load({ params }) {
 </script>
 
 <script>
+import { onMount } from "svelte";
+
 export let tool;
 export let version;
+
+// Load stats on page load
+let stats = {};
+onMount(async () => {
+	const response = await fetch(`/api/v3/stats/${tool.name}/${version.version}`).then(d => d.json());
+	const statsPerProgram = response.stats[tool.name];  // cat: { version: { date: ..., total: ... } }
+	for(let program in statsPerProgram)
+		stats[program] = statsPerProgram[program][version.version]?.total || 0;
+})
 </script>
 
 <base href="{CONFIG.url}/{tool.name}/{version.version}/" />
 
 <!-- Dependencies -->
 {#each version.dependencies || [] as dependency}
-	<a href="{CONFIG.url}/{dependency.name}/{dependency.version}">
-		<Badge pill color="primary" class="mb-4">Depends on {dependency.name} v{dependency.version}</Badge>
-	</a>
+	<Badge pill color="primary" class="mb-4">
+		Depends on
+		<a class="text-light" href="{CONFIG.url}/{dependency.name}/{dependency.version}">
+			{dependency.name} v{dependency.version}
+		</a>
+	</Badge>
 {/each}
 
 <!-- Sample Usage -->
@@ -38,7 +52,12 @@ export let version;
 <!-- Files to download -->
 <h5>Files</h5>
 {#each tool.programs || [tool.name] as program}
-	<h6 class="mt-3">{program}</h6>
+	<h6 class="mt-3">
+		{program}
+		{#if stats[program]}
+			<Badge pill color="secondary" class="ms-1">{stats[program]} downloads</Badge>
+		{/if}
+	</h6>
 	<ListGroup>
 		{#each tool.files || ["js", "wasm"] as extension}
 			<!-- Need full page reload to download the file -->
