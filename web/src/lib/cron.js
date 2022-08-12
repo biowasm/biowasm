dbfb8fd45b10266155952a8fbf3c5dd2ac59e6ca
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-08-12 09:46:31 -0700

    Add cron logic to save summary stats in KV

--- web/src/lib/cron.js
+++ web/src/lib/cron.js
@@ -1,8 +1,36 @@
-// Cron that gathers all stats from Durable Objects and stores a summary in a
-// KV pair. Note that BIOWASM_CONFIG is defined in `bin/postbuild.sh`.
+// Cron that gathers stats from all Durable Objects and stores a summary in a KV
+// for fast access. Note that BIOWASM_CONFIG is defined in `bin/postbuild.sh`.
 entry_default.scheduled = async (event, env, ctx) => {
-	console.log("CONFIG1", BIOWASM_CONFIG);
-	console.log("event", event);
-	console.log("env", env);
-	console.log("ctx", ctx);
+	console.log("Starting cron on env =", env.ENVIRONMENT);
+	ctx.waitUntil(cron(env));
 };
+
+// Cron logic
+async function cron(env) {
+	const stats = {};
+
+	// Loop through each tool
+	for(let tool of BIOWASM_CONFIG.tools) {
+		stats[tool.name] = {};
+
+		// Loop through each program
+		for(let program of tool.programs) {
+			stats[tool.name][program] = {};
+
+			// Loop through each version
+			for(let version of tool.versions) {
+				// Get Durable Object
+				const path = `${tool.name}/${version.version}/${program}.js`;
+				const id = env.stats.idFromName(path);
+				const obj = env.stats.get(id);
+
+				// Send HTTP request to Durable Object
+				const data = await (await obj.fetch(`https://biowasm-v3-${env.ENVIRONMENT}.robert.workers.dev`)).json();
+				stats[tool.name][program][version.version] = data;
+			}
+		}
+	}
+
+	// Save in KV store
+	await env.CDN.put("STATS", JSON.stringify(stats));
+}
