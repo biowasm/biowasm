f1eb165a9908da82f9016e28a3ba953432124c97
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-08-13 09:54:49 -0700

    Cleanup

--- web/src/components/Plotly.svelte
+++ web/src/components/Plotly.svelte
@@ -5,10 +5,10 @@ let elPlot;
 
 <svelte:head>
 	<script async src="https://cdn.plot.ly/plotly-2.14.0.min.js" on:load={() => {
-		// Zoom on data from 6 months ago
+		// Zoom on data from 3 months ago
 		let dateToday = new Date();
 		let dateSixMonthsAgo = new Date();
-		dateSixMonthsAgo.setMonth(dateToday.getMonth() - 6);
+		dateSixMonthsAgo.setMonth(dateToday.getMonth() - 3);
 
 		// Plot
 		const range = [dateSixMonthsAgo, dateToday].map(d => d.toISOString().slice(0, 10));
@@ -16,4 +16,4 @@ let elPlot;
 	}}></script>
 </svelte:head>
 
-<div bind:this={elPlot} style="width:600px; height:400px"></div>
+<div bind:this={elPlot} style="width:100%; height:400px"></div>
