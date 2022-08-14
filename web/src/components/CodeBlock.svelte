ea566f9c606ddcfcd42bf71491caa08c28360e50
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-08-14 06:59:19 -0700

    Add component for syntax highlighted code blocks

---
+++ web/src/components/CodeBlock.svelte
@@ -0,0 +1,27 @@
+<script>
+// Syntax-highlighted block of code
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import xml from "highlight.js/lib/languages/xml";
+import "highlight.js/styles/github.css";
+
+// Register languages
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("xml", xml);
+
+// State
+export let code = "<strong>Hello</strong> World.";
+export let lang = "xml";
+
+// Trim extra whitespace before highlighting the code
+const codeTrimmed = code.split("\n").map(d => d.trim()).join("\n").trim();
+const codeHighlighted = hljs.highlight(codeTrimmed, { language: lang }).value;
+</script>
+
+<pre>{@html codeHighlighted}</pre>
+
+<style>
+pre {
+	margin-left: 20px;
+}
+</style>
