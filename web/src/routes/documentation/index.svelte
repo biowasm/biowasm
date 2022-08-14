318977256fc9154fa1a2fca0696c58fdb8897de1
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-08-13 17:02:51 -0700

    Add highlight.js for syntax highlighting in docs

--- web/src/routes/documentation/index.svelte
+++ web/src/routes/documentation/index.svelte
@@ -1,11 +1,21 @@
 <script>
 import { browser } from "$app/env";
 import { Alert, ListGroup, ListGroupItem } from "sveltestrap";
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import xml from 'highlight.js/lib/languages/xml';
+import bash from 'highlight.js/lib/languages/bash';
+import 'highlight.js/styles/github.css';
+
 import CodePen from "$components/CodePen.svelte";
 import codeSimple from "$examples/simple.html?raw";
 import codeUserFiles from "$examples/files-local.html?raw";
 import codeRemoteFiles from "$examples/files-remote.html?raw";
 import codeMultipleTools from "$examples/multiple-tools.html?raw";
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('bash', bash);
 </script>
 
 <svelte:head>
@@ -23,6 +33,8 @@ import codeMultipleTools from "$examples/multiple-tools.html?raw";
 		<ListGroupItem tag="a" href="#local-files">Process local user files</ListGroupItem>
 		<ListGroupItem tag="a" href="#remote-files">Process remote user files</ListGroupItem>
 		<ListGroupItem tag="a" href="#multiple-tools">Run multiple tools</ListGroupItem>
+		<ListGroupItem tag="a" href="#no-cdn">Biowasm without a CDN</ListGroupItem>
+		<ListGroupItem tag="a" href="#aioli">Aioli API</ListGroupItem>
 		<ListGroupItem></ListGroupItem>
 	</ListGroup>
 
@@ -76,6 +88,53 @@ import codeMultipleTools from "$examples/multiple-tools.html?raw";
 	{#if browser}
 		<CodePen code={codeMultipleTools} /><br />
 	{/if}
+	<!-- No CDN -->
+	<h5 id="no-cdn">Use biowasm without the CDN</h5>
+	<p>
+		The examples above use the biowasm CDN, which is where the WebAssembly assets for the genomics tools are hosted for free. Most tools that use biowasm rely on the CDN, but if you would like to store your assets alongside your app, here's how to do so:
+	</p>
+
+	<h6>Step 1: Install the Aioli package</h6>
+	<p>Instead of importing Aioli using</p>
+	<pre>{@html hljs.highlight(`<script src="https://biowasm.com/cdn/v3/aioli.js"></script>`, {language: "xml"}).value}</pre>
+	<p>you can install Aioli with npm:</p>
+	<pre>{@html hljs.highlight(`npm install --save "@biowasm/aioli"`, {language: "bash"}).value}</pre>
+	<p>Then you can import Aioli as follows:</p>
+	<pre>{@html hljs.highlight(`import Aioli from "@biowasm/aioli";`, {language: "javascript"}).value}</pre>
+
+	<h6>Step 2: Download Biowasm assets</h6>
+	<p>
+		Note that even if you import Aioli locally with <code>npm</code>, the WebAssembly modules will still be downloaded from the biowasm CDN unless you download those assets locally as well.
+	</p>
+	<p>
+		To do so, navigate to the <a href="/cdn/v3">Packages page</a> and download the files for each tool of interest.
+	</p>
+
+	<h6>Step 3: Set up Aioli</h6>
+	<p>Once your assets are downloaded, you need to let Aioli know the relative URL path where it can find those assets from your server. Use the config parameter <code>urlPrefix</code> as shown in the <a href="#aioli-init-advanced:~:text=urlPrefix%3A%20%22./assets/%22%2C">Aioli API</a> section below.</p>
+	<!-- Aioli -->
+	<h5 id="aioli">Aioli API</h5>
+	<h6 id="aioli-init-simple">Simple Initialization</h6>
+	<p>Most tools can be initialized using tool names and their versions:</p>
+	<pre>{@html hljs.highlight(`new Aioli(["samtools/1.10", "seqtk/1.3"]);`, {language: "javascript"}).value}</pre>
+	<p>For tools that include multiple sub-tools, you need to specify which ones you want:</p>
+	<pre>{@html hljs.highlight(`new Aioli(["coreutils/head/8.32", "coreutils/tail/8.32"]);`, {language: "javascript"}).value}</pre>
+
+	<h6 id="aioli-init-advanced">Advanced Initialization</h6>
+	<p>If you need to customize the URL where the WebAssembly assets are stored or the lazy-loading behavior of a module:</p>
+	<pre>{@html hljs.highlight(`new Aioli([{
+    tool: "coreutils",
+    version: "8.32",
+    program: "head",         // Optional: sub-tool name; not needed for most tools (default: same as tool name)
+    urlPrefix: "./assets/",  // Optional: custom path to .wasm assets (default: biowasm CDN)
+    loading: "lazy",         // Optional: if set to "lazy", only downloads WebAssembly modules when needed, not at initialization (default: eager)
+    reinit: false,           // Optional: if set to true, will reinitialize module after each invocation; not needed for most tools
+}], {
+    printInterleaved: true,  // Optional: whether to return interleaved stdout/stderr; if false, returns object with stdout/stderr keys (default: true)
+    debug: false,            // Optional: set to true to see console log messages for debugging (default: false)
+}
+);`, {language: "javascript"}).value}</pre>
+
 </div>
 
 <style>
@@ -83,7 +142,15 @@ h5 {
 	margin-top: 40px;
 }
 
+h6 {
+	margin-top: 20px;
+}
+
 p {
 	line-height: 1.7em;
+}
+
+pre {
+	margin-left: 20px;
 }
 </style>
