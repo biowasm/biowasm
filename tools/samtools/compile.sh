3e74d1f3d42da49fcd2af1b85decd7c29ed31d2a
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-08-15 07:08:49 -0700

    Cleanup

--- tools/samtools/compile.sh
+++ tools/samtools/compile.sh
@@ -2,8 +2,6 @@
 
 # TODO: look into LZMA support
 
-test -d ../htslib/build/ || (echo "Running 'make htslib' first." && cd ../../ && make htslib && cd tools/samtools/)
-
 make clean
 
 # Also, use autoheader/autoconf to generate config.h.in and configure
