ed5582c56d87e9469ff5a215a212e4d4bc89f606
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-08-18 14:24:08 -0700

    Add Cypress config

---
+++ web/cypress.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+	fixturesFolder: false,
+	video: false,
+	screenshotOnRunFailure: false,
+	defaultCommandTimeout: 10000,
+	fileServerFolder: "../",
+	e2e: {
+		experimentalSessionAndOrigin: true,
+		specPattern: 'tests//**/*.cy.js',
+		supportFile: false
+	},
+});
