8ad2ee507038b994cfe0c218b152c98ebd707a89
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-10-10 10:40:12 -0700

    Add -maxItems=N for bigWigToWig

--- tools/kentutils/compile.sh
+++ tools/kentutils/compile.sh
@@ -11,7 +11,7 @@ cd bigBedToBed
 emcc -O2 -std=c99 -D_LARGEFILE_SOURCE -D_GNU_SOURCE -DMACHTYPE_x86_64 -DUSE_HIC -I../../inc -I../../htslib -o bigBedToBed.o -c bigBedToBed.c
 
 emcc -O2 -o bigBedToBed.html bigBedToBed.o ../../lib/x86_64/jkweb.a ../../htslib/libhts.a -lm \
-  -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s USE_ZLIB=1 --preload-file test.bb -s EXPORTED_RUNTIME_METHODS=["callMain","FS"]
+  -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s USE_ZLIB=1 --preload-file test.bb -s EXPORTED_RUNTIME_METHODS=["callMain","FS"] -s INVOKE_RUN=0 -s ALLOW_MEMORY_GROWTH=1
 
 cd ..
 
@@ -21,6 +21,6 @@ cd bigWigToWig
 emcc -O2 -std=c99 -D_LARGEFILE_SOURCE -D_GNU_SOURCE -DMACHTYPE_x86_64 -DUSE_HIC -I../../inc -I../../htslib -o bigWigToWig.o -c bigWigToWig.c
 
 emcc -O2 -o bigWigToWig.html bigWigToWig.o ../../lib/x86_64/jkweb.a ../../htslib/libhts.a -lm \
-  -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s USE_ZLIB=1 --preload-file test.bw -s EXPORTED_RUNTIME_METHODS=["callMain","FS"]
+  -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s USE_ZLIB=1 --preload-file test.bw -s EXPORTED_RUNTIME_METHODS=["callMain","FS"] -s INVOKE_RUN=0 -s ALLOW_MEMORY_GROWTH=1
 
 cd ..
