0166e2e5afe8dbce319a616a66bf32b24b6e40aa
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-10-11 08:18:59 -0700

    Clean up kentutils compile script

--- tools/kentutils/compile.sh
+++ tools/kentutils/compile.sh
@@ -1,26 +1,22 @@
-cd src/
-
 # Compile libraries kentutils depends on (jkweb.a, jkOwnLib.a, paralib.a, embedded htslib)
+cd src/
 emmake make topLibs
 
 # Compile tools
 cd utils/
+TOOLS=("bigBedToBed" "bigWigToWig")
+for TOOL in ${TOOLS[@]};
+do
+  cd $TOOL
 
-cd bigBedToBed
+  emcc -O2 -o $TOOL.o -c $TOOL.c \
+    -std=c99 -I../../inc -I../../htslib \
+    -D_LARGEFILE_SOURCE -D_GNU_SOURCE -DMACHTYPE_x86_64 -DUSE_HIC
 
-emcc -O2 -std=c99 -D_LARGEFILE_SOURCE -D_GNU_SOURCE -DMACHTYPE_x86_64 -DUSE_HIC -I../../inc -I../../htslib -o bigBedToBed.o -c bigBedToBed.c
+  emcc -O2 -o ../../../../build/$TOOL.js $TOOL.o \
+    ../../lib/x86_64/jkweb.a ../../htslib/libhts.a -lm \
+    -s ERROR_ON_UNDEFINED_SYMBOLS=0 $EM_FLAGS
 
-emcc -O2 -o bigBedToBed.html bigBedToBed.o ../../lib/x86_64/jkweb.a ../../htslib/libhts.a -lm \
-  -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s USE_ZLIB=1 --preload-file test.bb -s EXPORTED_RUNTIME_METHODS=["callMain","FS"] -s INVOKE_RUN=0 -s ALLOW_MEMORY_GROWTH=1
+  cd ..
+done
 
-cd ..
-
-
-cd bigWigToWig
-
-emcc -O2 -std=c99 -D_LARGEFILE_SOURCE -D_GNU_SOURCE -DMACHTYPE_x86_64 -DUSE_HIC -I../../inc -I../../htslib -o bigWigToWig.o -c bigWigToWig.c
-
-emcc -O2 -o bigWigToWig.html bigWigToWig.o ../../lib/x86_64/jkweb.a ../../htslib/libhts.a -lm \
-  -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s USE_ZLIB=1 --preload-file test.bw -s EXPORTED_RUNTIME_METHODS=["callMain","FS"] -s INVOKE_RUN=0 -s ALLOW_MEMORY_GROWTH=1
-
-cd ..
