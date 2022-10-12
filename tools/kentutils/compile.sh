a509b91256625dcca162652d2ceee926d88d2365
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-10-12 07:23:03 -0700

    Cleanup

--- tools/kentutils/compile.sh
+++ tools/kentutils/compile.sh
@@ -4,14 +4,14 @@ emmake make topLibs
 
 # Compile tools
 cd utils/
-TOOLS=("bigBedToBed" "bigBedInfo" "bigBedHead" "bigWigToWig" "bigWigInfo" "bigWigHead")
+TOOLS=("bigBedToBed" "bigBedInfo" "bigWigToWig" "bigWigInfo")
 for TOOL in ${TOOLS[@]};
 do
   cd $TOOL
 
   emcc -O2 -o $TOOL.o -c $TOOL.c \
     -std=c99 -I../../inc -I../../htslib \
-    -D_LARGEFILE_SOURCE -D_GNU_SOURCE -DMACHTYPE_x86_64 -DUSE_HIC
+    -D_LARGEFILE_SOURCE -D_GNU_SOURCE -DMACHTYPE_x86_64
 
   emcc -O2 -o ../../../../build/$TOOL.js $TOOL.o \
     ../../lib/x86_64/jkweb.a ../../htslib/libhts.a -lm \
