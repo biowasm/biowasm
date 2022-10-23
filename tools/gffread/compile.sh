660f77f33200179ead40972ad47c20dd1dcdfe54
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-10-23 06:46:01 -0700

    Add sample GTF/GFF files

--- tools/gffread/compile.sh
+++ tools/gffread/compile.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 emmake make release CXX=em++ LINKER=em++ LDFLAGS=
-em++ -o ../build/gffread.js ../gclib/*.o gff_utils.o gffread.o -O2 $EM_FLAGS
+em++ -o ../build/gffread.js ../gclib/*.o gff_utils.o gffread.o -O2 $EM_FLAGS \
+    --preload-file examples/transcripts.gtf@/gffread/transcripts.gtf \
+    --preload-file examples/annotation.gff@/gffread/annotation.gff
