1ed44f0163cebd1f301ff18ff643bbba1995750f
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2023-05-16 08:01:58 -0700

    Add assets

--- tools/ogdf/compile.sh
+++ tools/ogdf/compile.sh
@@ -7,4 +7,12 @@ emmake make
 
 # CMakeFiles/ex-energybased.dir/doc/examples/layout/energybased.cpp.o
 emmake make doc/examples/layout/energybased.o
-em++ -o deleteme.js -L./ CMakeFiles/ex-energybased.dir/doc/examples/layout/energybased.cpp.o -lOGDF -lCOIN $EM_FLAGS
+em++ -o ../examples/energybased.js \
+    -L./ CMakeFiles/ex-energybased.dir/doc/examples/layout/energybased.cpp.o \
+    -lOGDF -lCOIN $EM_FLAGS
+
+    # --preload-file doc/examples/layout/sierpinski_04.gml@/ogdf/examples/sierpinski_04.gml
+    # --preload-file doc/examples/layout/ERDiagram.gml@/ogdf/examples/ERDiagram.gml \
+    # --preload-file doc/examples/layout/uk_Pack_Bary_EC_FRENC.gml@/ogdf/examples/uk_Pack_Bary_EC_FRENC.gml \
+    # --preload-file doc/examples/layout/unix-history-time.gml@/ogdf/examples/unix-history-time.gml \
+    # --preload-file doc/examples/layout/unix-history.gml@/ogdf/examples/unix-history.gml
