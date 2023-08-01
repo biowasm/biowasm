be41ddf3f064c7dfa0d168d11f8bb71e02f31c3c
Daniel Ji <jidaniel1234@gmail.com>
2023-07-31 22:57:35 -0700

    Add ViralConsensus 0.0.3

--- tools/ViralConsensus/compile.sh
+++ tools/ViralConsensus/compile.sh
@@ -1,6 +1,8 @@
+sed -i 's/unsigned char \*tmp = realloc(b->data, len);/unsigned char \*tmp = static_cast<unsigned char \*>(realloc(b->data, len));/g' htslib/cram/cram_io.h
+
 emmake make \
     CXX=em++ \
     EXE=../build/viral_consensus.js \
     HTSLIB_A=../../htslib/src/libhts.a \
     LIBS="-lz -lbz2" \
-    CXXFLAGS="-pedantic -std=c++11 $EM_FLAGS"
+    CXXFLAGS="-std=c++11 -fpermissive $EM_FLAGS"
