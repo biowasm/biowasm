ef6f092b47ca247ac33c592043ce12e332221d9a
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2023-08-05 21:39:57 -0700

    fastp: use Emscripten zlib instead of the one in the repo to avoid issues with .gz files + update tests

--- tools/fastp/compile.sh
+++ tools/fastp/compile.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Set `DYNAMIC_ZLIB` so we don't use the zlib included in the repo, which 
+# otherwise causes the warning:
+#   wasm-ld: warning: function signature mismatch: gzoffset
+#   >>> defined as (i32) -> i32 in ./obj/fastqreader.o
+#   >>> defined as (i32) -> i64 in /emsdk/upstream/emscripten/cache/sysroot/lib/wasm32-emscripten/libz.a(gzlib.c.o)
+# As a result, fastp will crash if you run it on a .fastq.gz file.
 cp ../data/NA12878.fastq.gz testdata/
 emmake make \
     TARGET="../build/fastp.js" \
-    LIBS="-O3 -s USE_ZLIB=1 $EM_FLAGS --preload-file testdata@/fastp/testdata -s ASSERTIONS=1"
+    CXXFLAGS="-std=c++11 -O3 -DDYNAMIC_ZLIB" \
+    LIBS="-s USE_ZLIB=1 $EM_FLAGS --preload-file testdata@/fastp/testdata"
