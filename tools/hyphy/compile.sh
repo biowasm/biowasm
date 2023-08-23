7cd2aa451e6a781d66af1f10d6c11c44c01958ee
Steven Weaver <sweaver@temple.edu>
2023-08-23 09:27:11 -0600

    updating em_flags for v2 compatibility

--- tools/hyphy/compile.sh
+++ tools/hyphy/compile.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-EM_FLAGS="-sSTACK_SIZE=2097152 -g4 -sASSERTIONS=1"
+EM_FLAGS="-sTOTAL_STACK=2097152 -02 -sASSERTIONS=1"
 
 # Remove extra space in front of Emscripten "-s" variables to avoid errors
-emcmake cmake -DCMAKE_EXE_LINKER_FLAGS="${EM_FLAGS//-s /-s}" .
+emcmake cmake -DCMAKE_EXE_LINKER_FLAGS="${EM_FLAGS//-s /-s} --preload-file res@/hyphy --preload-file tests@/hyphy" .
 emmake make hyphy
+emmake make install
 mv hyphy.{js,wasm} ../build/
