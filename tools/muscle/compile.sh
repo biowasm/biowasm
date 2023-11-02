7da7c08aead6bce901a98ae5656f42f50cdc47a5
orangeSi <ilikeorangeapple@gmail.com>
2023-11-02 12:44:38 -0400

    add muscle v3.8.1551

--- tools/muscle/compile.sh
+++ tools/muscle/compile.sh
@@ -1,4 +1,19 @@
-cd src/
-emmake make  \
-    CC=emcc CXX=em++
+
+## below is only for muscle v5.1.0
+#cd src/
+#git apply ../patch/5.1.0.patch
+#emmake make  CC=emcc CXX=em++
+
+
+## below is only for muscle v3.8.1551
+# Defines the env variable $EM_FLAGS
+source ../../bin/shared.sh
+mkdir -p src build
+wget https://drive5.com/muscle/muscle_src_3.8.1551.tar.gz
+tar xf muscle_src_3.8.1551.tar.gz -C src
+sed -i -r -e 's/strip muscle/#strip muscle/' -e 's/ -o muscle / -o ..\/build\/muscle.js \$\(EM_FLAGS\) /' src/Makefile
+cd src
+emmake make GPP=emcc EM_FLAGS="$EM_FLAGS"
+cd ..
+rm muscle_src_3.8.1551.tar.gz
 
