e0e8ffbb81398297cd839270608f276672a60a05
Steven Weaver <sweaver@temple.edu>
2023-11-06 11:25:31 -0600

    update hyphy version to 2.5.56

--- tools/hyphy/compile.sh
+++ tools/hyphy/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Remove extra space in front of Emscripten "-s" variables to avoid errors
-emcmake cmake -DCMAKE_EXE_LINKER_FLAGS="-sTOTAL_STACK=2097152 -02 -sASSERTIONS=1 ${EM_FLAGS//-s /-s} --preload-file res@/hyphy --preload-file tests@/hyphy" .
+emcmake cmake -DCMAKE_EXE_LINKER_FLAGS="-sTOTAL_STACK=2097152 -02 -sASSERTIONS=1 -sMODULARIZE=1 -sALLOW_MEMORY_GROWTH -sFORCE_FILESYSTEM=1 -sEXIT_RUNTIME=0 -s EXPORTED_RUNTIME_METHODS=["callMain","FS","PROXYFS","WORKERFS","UTF8ToString","getValue","AsciiToString"] -lworkerfs.js -lproxyfs.js -s INVOKE_RUN=0 -s ENVIRONMENT="web,worker" ${EM_FLAGS//-s /-s} -fwasm-exceptions --preload-file res@/hyphy --preload-file tests/hbltests@/tests"
 emmake make hyphy
 emmake make install
 mv hyphy.{js,wasm,data} ../build/
