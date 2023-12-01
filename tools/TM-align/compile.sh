9ba03800b22230a223dd1f29a67df951c874bd3e
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2023-11-30 20:20:10 -0800

    Update compile.sh

--- tools/TM-align/compile.sh
+++ tools/TM-align/compile.sh
@@ -1,8 +1,11 @@
-mkdir -p build src/testdata
-cd src/testdata
-wget -c "https://zhanggroup.org/TM-align/example/strA.pdb"
-wget -c "https://zhanggroup.org/TM-align/example/strB.pdb"
-cd ..
+# Download test data
+mkdir -p testdata
+cd testdata
+wget -c "https://zhanggroup.org/TM-align/example/strA.pdb" "https://zhanggroup.org/TM-align/example/strB.pdb"
+cd -
+
+# Download code and compile it
 wget -c "https://zhanggroup.org/TM-align/TMalign.cpp"
-em++ -static -O3 -ffast-math -lm $EM_FLAGS --preload-file  testdata@/TMalign/testdata -o ../build/TMalign.js TMalign.cpp
-echo done
+em++ -static -O3 -ffast-math -lm $EM_FLAGS \
+  --preload-file testdata@/TMalign/testdata
+  -o ../build/TMalign.js TMalign.cpp
