e01701afd3a47889fcd8304d491be798a5b92e3b
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2023-11-30 20:22:36 -0800

    Update compile.sh

--- tools/TM-align/compile.sh
+++ tools/TM-align/compile.sh
@@ -7,5 +7,5 @@ cd -
 # Download code and compile it
 wget -c "https://zhanggroup.org/TM-align/TMalign.cpp"
 em++ -static -O3 -ffast-math -lm $EM_FLAGS \
-  --preload-file testdata@/TMalign/testdata
+  --preload-file testdata@/TMalign/testdata \
   -o ../build/TMalign.js TMalign.cpp
