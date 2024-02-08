dc314154331b11d69599e1782f13f8a8c763b5d4
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2024-02-08 13:27:56 -0800

    Update samtools compile script to use LZMA library

--- tools/samtools/compile.sh
+++ tools/samtools/compile.sh
@@ -2,12 +2,18 @@
 
 make clean
 
-# Also, use autoheader/autoconf to generate config.h.in and configure
+# LZMA library flags
+LZMA_VERSION="5.2.5"
+DIR_LZMA=../../htslib/src/xz-${LZMA_VERSION}/src/liblzma
+CFLAGS_LZMA="-I${DIR_LZMA}/api -I${DIR_LZMA}/api/lzma"
+LFDLAGS_LZMA="-L${DIR_LZMA}/.libs"
+
+# Use autoheader/autoconf to generate config.h.in and configure
 autoheader
 autoconf -Wno-syntax
 emconfigure ./configure --without-curses --with-htslib="../../htslib/src/" CFLAGS="-s USE_ZLIB=1 -s USE_BZIP2=1"
 
 # Build
 emmake make samtools CC=emcc AR=emar \
-    CFLAGS="-O2 -s USE_ZLIB=1 -s USE_BZIP2=1" \
-    LDFLAGS="$EM_FLAGS --preload-file examples/@/samtools/examples/ -s ERROR_ON_UNDEFINED_SYMBOLS=0 -O2"
+    CFLAGS="-O2 -s USE_ZLIB=1 -s USE_BZIP2=1 $CFLAGS_LZMA" \
+    LDFLAGS="$EM_FLAGS --preload-file examples/@/samtools/examples/ -s ERROR_ON_UNDEFINED_SYMBOLS=0 -O2 $LFDLAGS_LZMA"
