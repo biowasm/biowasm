abb45582b5f6d887c9747bfef77f41c010ccb952
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2024-03-09 20:03:36 -0800

    Test compiling ViralConsensus differently for >= 0.0.4

--- tools/ViralConsensus/compile.sh
+++ tools/ViralConsensus/compile.sh
@@ -1,9 +1,24 @@
-# Because Emscripten does not support the -fpermissive flag, we need to modify htslib source code so that it compiles without errors.
-sed -i 's/unsigned char \*tmp = realloc(b->data, len);/unsigned char \*tmp = static_cast<unsigned char \*>(realloc(b->data, len));/g' htslib/cram/cram_io.h
+version=$(git describe --tags)
+is_less_than_version_0_0_4=$([[ "0.0.4" != $(echo -e "$version\n0.0.4" | sort -V | head -n 1) ]] && echo true)
 
-emmake make \
-    CXX=em++ \
-    EXE=../build/viral_consensus.js \
-    HTSLIB_A=../../htslib/src/libhts.a \
-    LIBS="-lz -lbz2 -llzma $LDFLAGS_LZMA" \
-    CXXFLAGS="-std=c++11 -fpermissive $EM_FLAGS $CFLAGS_LZMA"
+# ViralConsensus < 0.0.4
+if [[ "$is_less_than_version_0_0_4" == "true" ]]; then
+    # Because Emscripten does not support the -fpermissive flag, we need to modify htslib source code so that it compiles without errors.
+    sed -i 's/unsigned char \*tmp = realloc(b->data, len);/unsigned char \*tmp = static_cast<unsigned char \*>(realloc(b->data, len));/g' htslib/cram/cram_io.h
+
+    emmake make \
+        CXX=em++ \
+        EXE=../build/viral_consensus.js \
+        HTSLIB_A=../../htslib/src/libhts.a \
+        LIBS="-lz -lbz2 -llzma $LDFLAGS_LZMA" \
+        CXXFLAGS="-std=c++11 -fpermissive $EM_FLAGS $CFLAGS_LZMA"
+
+# ViralConsensus >= 0.0.4 doesn't rely on local htslib
+else 
+    emmake make \
+        CXX=em++ \
+        EXE=../build/viral_consensus.js \
+        INCLUDE=../../htslib/src/ \
+        LIBS="-lz -lbz2" \
+        CXXFLAGS="-Wall -pedantic -std=c++11 $EM_FLAGS"
+fi
