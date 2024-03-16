4ebf60d58599d40d0d79085886ff51d2418d8d2f
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2024-03-15 20:14:28 -0700

    mafft: small fixes

--- tools/mafft/compile.sh
+++ tools/mafft/compile.sh
@@ -1,7 +1,2 @@
-
-set -eu
-export EM_FLAGS=" -s USE_PTHREADS=0 $EM_FLAGS "
-
 cd core
-emmake make CC="emcc -s TOTAL_MEMORY=360MB  -s ASSERTIONS=1 $EM_FLAGS "
-cd ..
+emmake make CC="emcc -s TOTAL_MEMORY=360MB  -s ASSERTIONS=1 -s USE_PTHREADS=0 $EM_FLAGS"
