9d7b20671c54cfcd7c2a218874ecef708cec1748
Niema Moshiri <niemamoshiri@gmail.com>
2024-05-14 22:35:03 -0700

    Update compile.sh

--- tools/cawlign/compile.sh
+++ tools/cawlign/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Remove extra space in front of Emscripten "-s" variables to avoid errors
-emcmake cmake -DCMAKE_EXE_LINKER_FLAGS="${EM_FLAGS//-s /-s} --preload-file test@/cawlign" .
+emcmake cmake -DCMAKE_EXE_LINKER_FLAGS="${EM_FLAGS//-s /-s} --preload-file test@/cawlign --preload-file res@/cawlign" .
 emmake make cawlign
 mv cawlign.{js,wasm,data} ../build/
