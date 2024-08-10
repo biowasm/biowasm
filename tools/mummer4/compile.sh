8d40e1802c6099689d8b2bde92e3a0f9b6b337a5
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2024-08-09 20:36:29 -0700

    Small fix

--- tools/mummer4/compile.sh
+++ tools/mummer4/compile.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-BRANCH_OR_TAG=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)
-TARBALL=https://github.com/mummer4/mummer/releases/download/v$BRANCH_OR_TAG/mummer-$BRANCH_OR_TAG.tar.gz
-
 # Download source from release to avoid needing yaggo and autotools
+TARBALL=https://github.com/mummer4/mummer/releases/download/v4.0.0rc1/mummer-4.0.0rc1.tar.gz
 mkdir -p src_release
 curl -L -o mummer.tar.gz $TARBALL
 tar -xzvf mummer.tar.gz -C src_release --strip-components=1
