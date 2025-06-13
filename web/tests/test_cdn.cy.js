778e2a681645719438e3f8b214912ff7df53cfcf
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2025-06-13 11:11:53 -0700

    Fix CDN tests

--- web/tests/test_cdn.cy.js
+++ web/tests/test_cdn.cy.js
@@ -31,6 +31,11 @@ const EXPECTED_OUTPUTS = {
 		`Usage: htsfile [-chHv] FILE...`,
 		`Usage:   bgzip [OPTIONS] [FILE] ...`
 	],
+	"tools/htslib/examples/1.21.html": [
+		`Usage:   tabix [OPTIONS] [FILE] [REGION [...]]`,
+		`Usage: htsfile [-chHv] FILE...`,
+		`Usage:   bgzip [OPTIONS] [FILE] ...`
+	],
 	"tools/hyphy/examples/2.5.57.html": [`HYPHY 2.5.57(MP) for Emscripten on wasm32`],
 	"tools/ivar/examples/v1.3.1.html": [`r001	163	ref	7	30	8M4I4M1D3M	=	37	39	TTAGATAAAGAGGATACTG	*	XX:B:S,12561,2,20,112\nr002	0	ref	9	30	1S2I6M1P1I1P1I4M2I	*	0	0	AAAAGATAAGGGATAAA	*\nr003	0	ref	9	30	5H6M	*	0	0	AGCTAA	*\nr004	0	ref	16	30	6M14N1I5M	*	0	0	ATAGCTCTCAGC	*\nr003	16	ref	29	30	6H5M	*	0	0	TAGGC	*\nr001	83	ref	37	30	9M	=	7	-39	CAGCGCCAT	*`],
 	"tools/jq/examples/jq-1.6.html": [`there`],
