1fdc2b44e73f7c9a3b3e750cec2848dd1a2a717f
Fan-iX <61686936+Fan-iX@users.noreply.github.com>
2026-01-03 16:16:37 +0800

    add ASTER to biowasm

---
+++ tools/ASTER/compile.sh
@@ -0,0 +1,5 @@
+mkdir -p data
+rm data/*
+cp -r example/multitree_genename.map example/multitree_genename.nw example/genetrees.tre_1.fas -t data/
+make -f Makefile.emcc LDFLAGS="$EM_FLAGS --preload-file data@/ASTER/example"
+mv bin/* ../build/
