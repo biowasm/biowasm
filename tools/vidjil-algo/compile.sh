9ccc4c6fefb7a4f70975cb93ec4a725c5f3dff4f
Mikaël Salson <mikael-s@users.noreply.github.com>
2026-02-11 05:57:00 +0100

    Add vidjil-algo (#140)
    * Add vidjil-algo
    
    * Reorder alphabetically
    
    * Update preload file path in compile.sh
    
    * Make sure .data files are included on the CDN
    
    * Small fixes
    
    * Added -O3 to reduce .js size
    
    * Weird characters were causing issues in test data
    
    ---------
    
    Co-authored-by: Robert Aboukhalil <robert.aboukhalil@gmail.com>

---
+++ tools/vidjil-algo/compile.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+EM_FLAGS="$EM_FLAGS -O3 --preload-file ../demo/Demo-X5.fa@/vidjil-algo/demo.fa"
+
+make clean
+make -C algo clean
+make germline
+make demo
+emmake make -C algo EM_FLAGS="$EM_FLAGS" wasm 
+
+mv vidjil-algo.* ../build
