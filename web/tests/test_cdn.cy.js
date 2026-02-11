cd70367769442ee596d078a51ae2dcf720e6eca4
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2026-02-10 20:56:31 -0800

    Weird characters were causing issues in test data

--- web/tests/test_cdn.cy.js
+++ web/tests/test_cdn.cy.js
@@ -86,7 +86,11 @@ const EXPECTED_OUTPUTS = {
 		`ID1,ID2,Distance\nB_FR_83_HXB2_ACC_K03455_5,B_US_83_RF_ACC_M17451,0.045156\nB_FR_83_HXB2_ACC_K03455_5,B_US_86_JRFL_ACC_U63632,0.0296218\nB_FR_83_HXB2_ACC_K03455_5,B_US_90_WEAU160_ACC_U21135,0.0327566\nB_US_83_RF_ACC_M17451,B_US_86_JRFL_ACC_U63632,0.048328\nB_US_86_JRFL_ACC_U63632,B_US_90_WEAU160_ACC_U21135,0.0408994\nD_CD_83_ELI_ACC_K03454_7,D_CD_83_NDK_ACC_M27323,0.0287246\nD_CD_83_NDK_ACC_M27323,D_CD_84_84ZR085_ACC_U88822,0.0491974`
 	],
 	"tools/tree/examples/2.0.4.html": ["11 directories, 8 files", "└── web_user", "├── stderr -> /dev/tty1"],
-    "tools/vidjil-algo/release-2025.12.html": ["==> 14 sequences\n  ==> junction detected in 14 reads (100%)\n  ==> found 14 windows in 14 reads (100% of 14 reads)\n  ==> ./out///demo.windows.fa\n  ==> keep 14 windows in 14 reads (100.000%)  \n  ==> 14 clones\n  ==> suggested edges in ./out///demo.edges\n#### Clone #001 –       1 reads – 7.14%  \n#### Clone #002 –       1 reads – 7.14%  \n#### Clone #003 –       1 reads – 7.14%  \n#### Clone #004 –       1 reads – 7.14%  \n#### Clone #005 –       1 reads – 7.14%  \n#### Clone #006 –       1 reads – 7.14%  \n#### Clone #007 –       1 reads – 7.14%  \n#### Clone #008 –       1 reads – 7.14%  \n#### Clone #009 –       1 reads – 7.14%  \n#### Clone #010 –       1 reads – 7.14%  \n#### Clone #011 –       1 reads – 7.14%  \n#### Clone #012 –       1 reads – 7.14%  \n#### Clone #013 –       1 reads – 7.14%  \n#### Clone #014 –       1 reads – 7.14%  \n#### end of clones\n  ==> ./out//demo.tsv     (AIRR output)\n  ==> ./out//demo.vidjil     (main output file, may be opened by the Vidjil web application)"],
+    "tools/vidjil-algo/release-2025.12.html": [
+		"junction detected in 14 reads (100%)",
+		"found 14 windows in 14 reads (100% of 14 reads)",
+		"1 reads – 7.14%"
+	],
 	"tools/ViralConsensus/examples/0.0.1.html": ["AAAGGTTTATACCTTCCCAGGTAACAAACCAACCAACTTTCGATCTC"],
 	"tools/ViralConsensus/examples/0.0.2.html": ["AAAGGTTTATACCTTCCCAGGTAACAAACCAACCAACTTTCGATCTC"],
 	"tools/ViralConsensus/examples/0.0.3.html": ["AAAGGTTTATACCTTCCCAGGTAACAAACCAACCAACTTTCGATCTC"],
