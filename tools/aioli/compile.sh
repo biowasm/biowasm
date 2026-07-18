56ef8acc7d97a7ac794fb572153f9b4bbb1b123e
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2026-07-18 21:01:14 +0200

    Install Node.js for Aioli JS library build
    Added installation of Node.js using nvm for building the Aioli JS library.

--- tools/aioli/compile.sh
+++ tools/aioli/compile.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Install a modern Node just for building the Aioli JS library.
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+nvm use 22
+
 # Build Aioli
 npm install
 npm run build
