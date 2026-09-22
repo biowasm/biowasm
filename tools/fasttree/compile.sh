8d385e86bfeb7b6b0b7703ca7dc43ebcb4bd9e0d
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2026-09-22 10:45:22 +0200

    FastTree: Add the SIMD version (need to patch malloc because not aligned)

--- tools/fasttree/compile.sh
+++ tools/fasttree/compile.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Compile with SIMD (FastTree requires no doubles if use SSE)
+emcc \
+	-O3 -finline-functions -funroll-loops -Wall \
+	-msse -msimd128 \
+	-o ../build/fasttree-simd.js old/FastTree-2.1.11.c -lm $EM_FLAGS
+
+# Compile without SIMD
 emcc \
 	-DNO_SSE -DUSE_DOUBLE -O3 -finline-functions -funroll-loops -Wall \
 	-o ../build/fasttree.js old/FastTree-2.1.11.c -lm $EM_FLAGS
