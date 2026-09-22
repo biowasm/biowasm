9c8d396d20feec735ee7eb3b56d31be9208e4071
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2026-09-22 11:12:23 +0200

    Add bowtie2 SIMD

--- tools/bowtie2/compile.sh
+++ tools/bowtie2/compile.sh
@@ -12,7 +12,24 @@ mv example/reads/reads_2.fq.tmp example/reads/reads_2.fq
 # Build:
 #   - NO_TBB=1: disable Threading Building Blocks library
 #   - POPCNT_CAPABILITY=0: popcnt (used in bt2_idx.h) is an assembly instruction; not supported by WebAssembly
+#   - SIMD=1: compile to SIMD instead of using SIMDe fallback (see patches/v2.4.2.patch)
+#   - TOTAL_STACK=5MB: future proofing in prep for biowasm v4, since future Emscripten reduce this a lot
+WASM_FLAGS="$EM_FLAGS --preload-file example/@/bowtie2/example/ -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0 -s TOTAL_STACK=5MB"
+
+# Compile with SIMD
+echo "Compiling with SIMD"
 emmake make bowtie2-align-s \
+    SIMD=1 \
+    PROGRAM="bowtie2-align-s-simd" \
     NO_TBB=1 \
     POPCNT_CAPABILITY=0 \
-    WASM_FLAGS="$EM_FLAGS --preload-file example/@/bowtie2/example/ -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0"
+    WASM_FLAGS="$WASM_FLAGS"
+
+# Compile without SIMD (use SIMDe to simulate SIMD support)
+echo "Compiling without SIMD"
+make clean
+emmake make bowtie2-align-s \
+    PROGRAM="bowtie2-align-s" \
+    NO_TBB=1 \
+    POPCNT_CAPABILITY=0 \
+    WASM_FLAGS="$WASM_FLAGS"
